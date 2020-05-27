8a6bd394328e6d64a2af9b70e054e3d458bfebae
0xsaxit <41426816+0xsaxit@users.noreply.github.com>
2020-05-27 14:23:01 -0700

    currency default

--- components/home/exchange.js
+++ components/home/exchange.js
@@ -11,7 +11,7 @@ let Global;
 let transak = new transakSDK({
   apiKey: '4fcd6904-706b-4aff-bd9d-77422813bbb7',  // Your API Key
   environment: 'STAGING', // STAGING/PRODUCTION
-  defaultCryptoCurrency: 'ETH',
+  defaultCryptoCurrency: 'MANA',
   walletAddress: '', // Your customer's wallet address
   themeColor: '000000', // App theme color
   fiatCurrency: '', // INR/GBP
