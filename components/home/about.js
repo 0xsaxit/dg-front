9b4b5d0fdc92a1d6de5947643fef36a107764161
0xsaxit <41426816+0xsaxit@users.noreply.github.com>
2020-06-16 14:27:20 -0700

    game direct links, shrink partners

--- components/home/about.js
+++ components/home/about.js
@@ -88,75 +88,81 @@ class About extends React.Component {
               <Grid>
                 <Grid.Row>
                   <Grid.Column computer={4} tablet={8} mobile={16}>
-                    <Card className="games-card">
-                      <Reveal animated="fade">
-                        <Reveal.Content visible>
-                          <Image
-                            className="card-image"
-                            src="https://res.cloudinary.com/dnzambf4m/image/upload/v1589058163/slots-1_qa9ced_a8rqpc_lcvhua.jpg"
-                          />
-                        </Reveal.Content>
-                        <Reveal.Content hidden>
-                          <Image
-                            className="card-image"
-                            src="https://res.cloudinary.com/dnzambf4m/image/upload/v1589058163/slots-2_tvfv6r_lowidv_vabcjn.jpg"
-                          />
-                        </Reveal.Content>
-                      </Reveal>
-                      <Card.Content>
-                        <Card.Header className="games-card-header">
-                          SLOTS
-                        </Card.Header>
-                      </Card.Content>
-                    </Card>
+                    <a href="https://play.decentraland.org/?position=-119%2C133&realm=fenrir-gold" target="_blank">
+                      <Card className="games-card">
+                        <Reveal animated="fade">
+                          <Reveal.Content visible>
+                            <Image
+                              className="card-image"
+                              src="https://res.cloudinary.com/dnzambf4m/image/upload/v1589058163/slots-1_qa9ced_a8rqpc_lcvhua.jpg"
+                            />
+                          </Reveal.Content>
+                          <Reveal.Content hidden>
+                            <Image
+                              className="card-image"
+                              src="https://res.cloudinary.com/dnzambf4m/image/upload/v1589058163/slots-2_tvfv6r_lowidv_vabcjn.jpg"
+                            />
+                          </Reveal.Content>
+                        </Reveal>
+                        <Card.Content>
+                          <Card.Header className="games-card-header">
+                            SLOTS
+                          </Card.Header>
+                        </Card.Content>
+                      </Card>
+                    </a>
                   </Grid.Column>
 
                   <Grid.Column computer={4} tablet={8} mobile={16}>
-                    <Card className="games-card">
-                      <Reveal animated="fade">
-                        <Reveal.Content visible>
-                          <Image
-                            className="card-image"
-                            src="https://res.cloudinary.com/dnzambf4m/image/upload/v1589058162/roulette-1_rmgcgr_evcxkj_rwjlcw.jpg"
-                          />
-                        </Reveal.Content>
-                        <Reveal.Content hidden>
-                          <Image
-                            className="card-image"
-                            src="https://res.cloudinary.com/dnzambf4m/image/upload/v1589058161/roulette-2_hamycv_mchxtp_cbscn1.jpg"
-                          />
-                        </Reveal.Content>
-                      </Reveal>
-                      <Card.Content>
-                        <Card.Header className="games-card-header">
-                          ROULETTE
-                        </Card.Header>
-                      </Card.Content>
-                    </Card>
+                    <a href="https://play.decentraland.org/?position=-119%2C133&realm=fenrir-gold" target="_blank">
+                      <Card className="games-card">
+                        <Reveal animated="fade">
+                          <Reveal.Content visible>
+                            <Image
+                              className="card-image"
+                              src="https://res.cloudinary.com/dnzambf4m/image/upload/v1589058162/roulette-1_rmgcgr_evcxkj_rwjlcw.jpg"
+                            />
+                          </Reveal.Content>
+                          <Reveal.Content hidden>
+                            <Image
+                              className="card-image"
+                              src="https://res.cloudinary.com/dnzambf4m/image/upload/v1589058161/roulette-2_hamycv_mchxtp_cbscn1.jpg"
+                            />
+                          </Reveal.Content>
+                        </Reveal>
+                        <Card.Content>
+                          <Card.Header className="games-card-header">
+                            ROULETTE
+                          </Card.Header>
+                        </Card.Content>
+                      </Card>
+                    </a>
                   </Grid.Column>
 
                   <Grid.Column computer={4} tablet={8} mobile={16}>
-                    <Card className="games-card">
-                      <Reveal animated="fade">
-                        <Reveal.Content visible>
-                          <Image
-                            className="card-image"
-                            src="https://res.cloudinary.com/dnzambf4m/image/upload/v1589058160/dice_fheuwk_t8hjf6_ydjuva.jpg"
-                          />
-                        </Reveal.Content>
-                        <Reveal.Content hidden>
-                          <Image
-                            className="card-image"
-                            src="https://res.cloudinary.com/dnzambf4m/image/upload/v1589058160/backgammon_mgb1hk_xojcpl_gede81.jpg"
-                          />
-                        </Reveal.Content>
-                      </Reveal>
-                      <Card.Content>
-                        <Card.Header className="games-card-header">
-                          BACKGAMMON
-                        </Card.Header>
-                      </Card.Content>
-                    </Card>
+                    <a href="https://play.decentraland.org/?position=85%2C-20&realm=fenrir-gold" target="_blank">
+                      <Card className="games-card">
+                        <Reveal animated="fade">
+                          <Reveal.Content visible>
+                            <Image
+                              className="card-image"
+                              src="https://res.cloudinary.com/dnzambf4m/image/upload/v1589058160/dice_fheuwk_t8hjf6_ydjuva.jpg"
+                            />
+                          </Reveal.Content>
+                          <Reveal.Content hidden>
+                            <Image
+                              className="card-image"
+                              src="https://res.cloudinary.com/dnzambf4m/image/upload/v1589058160/backgammon_mgb1hk_xojcpl_gede81.jpg"
+                            />
+                          </Reveal.Content>
+                        </Reveal>
+                        <Card.Content>
+                          <Card.Header className="games-card-header">
+                            BACKGAMMON
+                          </Card.Header>
+                        </Card.Content>
+                      </Card>
+                    </a>
                   </Grid.Column>
 
                   <Grid.Column computer={4} tablet={8} mobile={16}>
@@ -200,53 +206,54 @@ class About extends React.Component {
                   </h3>
                 </Grid.Row>
                 <Grid.Row>
-                  <Grid.Column computer={4} tablet={8} mobile={16}>
-                    <Card className="games-card">
-                      <a href="https://decentraland.org" target="_blank">
+                  <Grid.Column computer={3} tablet={8} mobile={16}>
+                    <a href="https://decentraland.org" target="_blank">
+                      <Card className="games-card">
                         <Image
                           className="partner-image"
                           src="https://res.cloudinary.com/dnzambf4m/image/upload/v1590094010/Group_62_jyzvs7.png"
                         />
-                      </a>
-                      <Card.Content>
-                        <Card.Header className="games-card-header">
-                          DECENTRALAND
-                        </Card.Header>
-                      </Card.Content>
-                    </Card>
+                        <Card.Content>
+                          <Card.Header className="games-card-header">
+                            DECENTRALAND
+                          </Card.Header>
+                        </Card.Content>
+                      </Card>
+                    </a>
                   </Grid.Column>
 
-                  <Grid.Column computer={4} tablet={8} mobile={16}>
-                    <Card className="games-card">
-                      <a href="https://matic.network" target="_blank">
+                  <Grid.Column computer={3} tablet={8} mobile={16}>
+                    <a href="https://matic.network" target="_blank">
+                      <Card className="games-card">
                         <Image
                           className="partner-image"
                           src="https://res.cloudinary.com/dnzambf4m/image/upload/v1590094008/Group_58_osm0ci.png"
                         />
-                      </a>
-                      <Card.Content>
-                        <Card.Header className="games-card-header">
-                          MATIC NETWORK
-                        </Card.Header>
-                      </Card.Content>
-                    </Card>
+                        <Card.Content>
+                          <Card.Header className="games-card-header">
+                            MATIC NETWORK
+                          </Card.Header>
+                        </Card.Content>
+                      </Card>
+                    </a>
                   </Grid.Column>
 
-                  <Grid.Column computer={4} tablet={8} mobile={16}>
-                    <Card className="games-card">
-                      <a href="https://makerdao.com" target="_blank">
+                  <Grid.Column computer={3} tablet={8} mobile={16}>
+                    <a href="https://makerdao.com" target="_blank">
+                      <Card className="games-card">
                         <Image
                           className="partner-image"
                           src="https://res.cloudinary.com/dnzambf4m/image/upload/v1590094013/Group_61_dkxigb.png"
                         />
-                      </a>
-                      <Card.Content>
-                        <Card.Header className="games-card-header">
-                          MAKER DAO
-                        </Card.Header>
-                      </Card.Content>
-                    </Card>
+                        <Card.Content>
+                          <Card.Header className="games-card-header">
+                            MAKER DAO
+                          </Card.Header>
+                        </Card.Content>
+                      </Card>
+                    </a>
                   </Grid.Column>
+
                 </Grid.Row>
               </Grid>
             </div>
@@ -258,3 +265,4 @@ class About extends React.Component {
 }
 
 export default About;
+
