5dcd0b989ff10ff88a605ac46011ebdb7a456be3
0xsaxit <41426816+0xsaxit@users.noreply.github.com>
2020-06-26 12:11:59 -0700

    fix

--- components/home/TransactionHistory.js
+++ components/home/TransactionHistory.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Icon } from 'semantic-ui-react';
+import { Icon, Divider } from 'semantic-ui-react';
 import Spinner from '../Spinner';
 import ContentTransactions from './ContentTransactions';
 // import Menu from './menu2';
