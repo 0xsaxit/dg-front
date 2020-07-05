c0eb08f314b97f71d086192ef569a82f7e486968
john doe <mockworld@gmail.com>
2020-07-06 03:50:46 +0700

    removed images folder

--- next.configDEPRICATED.js
+++
@@ -1,9 +0,0 @@
-const withImages = require('next-images');
-const path = require('path');
-console.log(__dirname);
-module.exports = withImages({
-  exclude: path.resolve(__dirname, 'static/images2'),
-  webpack(config, options) {
-    return config;
-  },
-});
--- store/apiDEPRICATED.js
+++
@@ -1,5 +0,0 @@
-import Butter from 'buttercms'
-
-const token = 'd7d6d8425656d3cfe5f45d7a0a3a8470ef09d434'
-
-export const butter = Butter(token)
