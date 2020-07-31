458c54c011d0b81c38416c25eb114e23844e9f0a
0xsaxit <41426816+0xsaxit@users.noreply.github.com>
2020-07-30 18:58:40 -0700

    dropdown menu

--- components/home/MenuTop.js
+++ components/home/MenuTop.js
@@ -2,7 +2,7 @@ import { useState, useContext } from 'react';
 import { GlobalContext } from '../../store';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
-import { Menu } from 'semantic-ui-react';
+import { Menu, Sidebar, Segment, Icon } from 'semantic-ui-react';
 import ModalVerify from '../modal/ModalVerify';
 import ModalDeposit from '../modal/ModalDeposit';
 import MessageBox from './MessageBox';
@@ -11,6 +11,7 @@ import Global from '../Constants';
 const MenuTop = () => {
   // get token balances from the Context API store
   const [state, dispatch] = useContext(GlobalContext);
+  const [visible, setVisible] = React.useState(false)
 
   /////////////////////////////////////////////////////////////////////////////////////////
   /////////////////////////////////////////////////////////////////////////////////////////
@@ -52,105 +53,173 @@ const MenuTop = () => {
       data: 0,
     });
   }
+  
+  /////////////////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////////////////
+  // open and close mobile dropdown menu 
+  function handleDimmedChange() {
+    if (visible === 0) {
+      setVisible(1);
+    } else {
+      setVisible(0);
+    }
+  }
 
   if (state.userStatus) console.log('User status: ' + state.userStatus);
 
   return (
-    <div className={getContainerStyles('container')}>
-      <Menu className={getLinkStyles('menu')} icon="labeled">
-        <Link href="/">
-          <img
-            className="image inline pointer"
-            src={Global.IMAGES.LOGO}
-            style={{
-              width: '39px',
-              paddingTop: '15px',
-              paddingBottom: '15px',
-              marginRight: '12px',
-              marginLeft: '24px',
-              height: '100%',
-            }}
-          />
-        </Link>
+    <div>
+      <div className="dropdown-menu" id={visible === 0 ? "pushable-one" : "pushable-two"}>
+        <Icon
+          name={ visible === 0 ? "sidebar" : "x" }
+          onClick={handleDimmedChange}
+          className="mobile-menu-icon"
+        />
+        <Sidebar.Pushable>
+          <Sidebar
+            as={Menu}
+            direction="top"
+            animation='overlay'
+            icon='labeled'
+            vertical
+            visible={visible}
+            style={{ backgroundColor: 'black' }}
+          >
+            
+            <Link href="/">
+              <Menu.Item className={getLinkStyles('/')}>PLAY</Menu.Item>
+            </Link>
 
-        <Link href="/">
-          <Menu.Item className={getLinkStyles('/')}>PLAY</Menu.Item>
-        </Link>
+            {state.userStatus ? (
+              <Link href="/account">
+                <Menu.Item className={getLinkStyles('/account')}>ACCOUNT</Menu.Item>
+              </Link>
+            ) : null}
 
-        {state.userStatus ? (
-          <Link href="/account">
-            <Menu.Item className={getLinkStyles('/account')}>ACCOUNT</Menu.Item>
+            <Link href="/games">
+              <Menu.Item className={getLinkStyles('/games')}>GAMES</Menu.Item>
+            </Link>
+
+            <Link href="/nfts">
+              <Menu.Item className={getLinkStyles('/nfts')}>NFTS</Menu.Item>
+            </Link>
+
+            <Link href="/blog">
+              <Menu.Item className={getLinkStyles('/blog')}>BLOG</Menu.Item>
+            </Link>
+
+            <Menu.Item
+              href="https://docs.decentral.games/"
+              target="_blank"
+              className="sidebar-menu-text"
+              style={{ paddingBottom: '27px' }}
+            >
+              DOCS
+            </Menu.Item>
+          </Sidebar>
+          <Sidebar.Pusher>
+            <Segment className="transparent-menu-segment">
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </div>
+
+      <div className={getContainerStyles('container')}>
+        <Menu className={getLinkStyles('menu')} icon="labeled">
+          <Link href="/">
+            <img
+              className="image inline pointer"
+              src={Global.IMAGES.LOGO}
+              style={{
+                width: '39px',
+                paddingTop: '15px',
+                paddingBottom: '15px',
+                marginRight: '12px',
+                marginLeft: '24px',
+                height: '100%',
+              }}
+            />
           </Link>
-        ) : null}
 
-        <Link href="/games">
-          <Menu.Item className={getLinkStyles('/games')}>GAMES</Menu.Item>
-        </Link>
+          <Link href="/">
+            <Menu.Item className={getLinkStyles('/')}>PLAY</Menu.Item>
+          </Link>
 
-        <Link href="/nfts">
-          <Menu.Item className={getLinkStyles('/nfts')}>NFTS</Menu.Item>
-        </Link>
+          {state.userStatus ? (
+            <Link href="/account">
+              <Menu.Item className={getLinkStyles('/account')}>ACCOUNT</Menu.Item>
+            </Link>
+          ) : null}
 
-        <Link href="/blog">
-          <Menu.Item className={getLinkStyles('/blog')}>BLOG</Menu.Item>
-        </Link>
+          <Link href="/games">
+            <Menu.Item className={getLinkStyles('/games')}>GAMES</Menu.Item>
+          </Link>
 
-        <Menu.Item
-          href="https://docs.decentral.games/"
-          target="_blank"
-          className="sidebar-menu-text"
-        >
-          DOCS
-        </Menu.Item>
+          <Link href="/nfts">
+            <Menu.Item className={getLinkStyles('/nfts')}>NFTS</Menu.Item>
+          </Link>
 
-        {state.userStatus ? (
-          /////////////////////////////////////////////////////////////////////////////////////////
-          /////////////////////////////////////////////////////////////////////////////////////////
-          // display token balances and 'ADD CRYPTO' button
-          <span className="right-menu-items">
-            <span className="sidebar-menu-text-2">
-              <img
-                style={{
-                  verticalAlign: 'middle',
-                  marginRight: '6px',
-                  marginTop: '-1px',
-                }}
-                className="image inline"
-                width="18px"
-                height="18px"
-                src={Global.IMAGES.DAI_CIRCLE}
-              />
-              {state.balances[1][1]} DAI
+          <Link href="/blog">
+            <Menu.Item className={getLinkStyles('/blog')}>BLOG</Menu.Item>
+          </Link>
+
+          <Menu.Item
+            href="https://docs.decentral.games/"
+            target="_blank"
+            className="sidebar-menu-text"
+          >
+            DOCS
+          </Menu.Item>
+
+          {state.userStatus ? (
+            /////////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////////////////////////////////////////
+            // display token balances and 'ADD CRYPTO' button
+            <span className="right-menu-items">
+              <span className="sidebar-menu-text-2">
+                <img
+                  style={{
+                    verticalAlign: 'middle',
+                    marginRight: '6px',
+                    marginTop: '-1px',
+                  }}
+                  className="image inline"
+                  width="18px"
+                  height="18px"
+                  src={Global.IMAGES.DAI_CIRCLE}
+                />
+                {state.balances[1][1]} DAI
+              </span>
+
+              <span className="sidebar-menu-text-3">
+                <img
+                  style={{
+                    verticalAlign: 'middle',
+                    marginRight: '6px',
+                    marginTop: '-1px',
+                  }}
+                  className="image inline"
+                  width="18px"
+                  height="18px"
+                  src={Global.IMAGES.MANA_CIRCLE}
+                />
+                {state.balances[0][1]} MANA
+              </span>
+
+              <ModalDeposit menuLink={1} />
             </span>
-
-            <span className="sidebar-menu-text-3">
-              <img
-                style={{
-                  verticalAlign: 'middle',
-                  marginRight: '6px',
-                  marginTop: '-1px',
-                }}
-                className="image inline"
-                width="18px"
-                height="18px"
-                src={Global.IMAGES.MANA_CIRCLE}
-              />
-              {state.balances[0][1]} MANA
+          ) : (
+            /////////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////////////////////////////////////////
+            // show 'CONNECT METAMASK' button
+            <span className="right-menu-items">
+              <ModalVerify />
             </span>
+          )}
+        </Menu>
 
-            <ModalDeposit menuLink={1} />
-          </span>
-        ) : (
-          /////////////////////////////////////////////////////////////////////////////////////////
-          /////////////////////////////////////////////////////////////////////////////////////////
-          // show 'CONNECT METAMASK' button
-          <span className="right-menu-items">
-            <ModalVerify />
-          </span>
-        )}
-      </Menu>
-
-      {state.messageBox ? <MessageBox handleDismiss={handleDismiss} /> : null}
+        {state.messageBox ? <MessageBox handleDismiss={handleDismiss} /> : null}
+      </div>
     </div>
   );
 };
