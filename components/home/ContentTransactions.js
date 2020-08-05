5615113e0c517a716c07a35bacc54658e050fa7d
0xsaxit <scott@flux.la>
2020-08-05 10:46:28 -0700

    padding fixes, remove grey backgrounds

--- components/home/ContentTransactions.js
+++ components/home/ContentTransactions.js
@@ -118,35 +118,6 @@ const ContentTransactions = (props) => {
             <Grid.Column computer={5} tablet={8} mobile={16} className="balances-column two">
               <span className="balances-text">
                 <img
-                  src={Global.IMAGES.MANA_CIRCLE}
-                  style={{
-                    width: '28px',
-                    marginRight: '9px',
-                    verticalAlign: 'top',
-                    marginTop: '-2px'
-                  }}
-                />
-                {state.balances[0][1]} MANA
-              </span>
-              <Divider style={{ marginTop: '23px', marginBottom: '30px' }}/>
-              <span className="balances-button-span">
-                <ModalDeposit menuLink={0} />
-                <ModalWithdraw isExit={0} />
-              </span>
-              <div>
-                <Button
-                  color="blue"
-                  className="balances-purchase-button"
-                  onClick={show_transak}
-                >
-                  PURCHASE
-                </Button>
-              </div>
-            </Grid.Column>
-
-            <Grid.Column computer={5} tablet={8} mobile={16} className="balances-column three">
-              <span className="balances-text">
-                <img
                   src={Global.IMAGES.DAI_CIRCLE}
                   style={{
                     width: '28px',
@@ -185,6 +156,35 @@ const ContentTransactions = (props) => {
                   className="balances-purchase-button"
                   href="https://play.decentral.games"
                   target="_blank"
+                >
+                  PURCHASE
+                </Button>
+              </div>
+            </Grid.Column>
+
+            <Grid.Column computer={5} tablet={8} mobile={16} className="balances-column three">
+              <span className="balances-text">
+                <img
+                  src={Global.IMAGES.MANA_CIRCLE}
+                  style={{
+                    width: '28px',
+                    marginRight: '9px',
+                    verticalAlign: 'top',
+                    marginTop: '-2px'
+                  }}
+                />
+                {state.balances[0][1]} MANA
+              </span>
+              <Divider style={{ marginTop: '23px', marginBottom: '30px' }}/>
+              <span className="balances-button-span">
+                <ModalDeposit menuLink={0} />
+                <ModalWithdraw isExit={0} />
+              </span>
+              <div>
+                <Button
+                  color="blue"
+                  className="balances-purchase-button"
+                  onClick={show_transak}
                 >
                   PURCHASE
                 </Button>
