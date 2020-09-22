8bc7529b2f05e09c62db0ba23544d575f0a7b9b5
0xsaxit <scott@flux.la>
2020-09-22 11:30:07 -0700

    dg page clean up, avatar pic background, deposit box

--- components/home/Farming.js
+++ components/home/Farming.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useContext } from 'react';
 import { GlobalContext } from '../../store';
-import { Image, Button, Divider } from 'semantic-ui-react';
+import { Image, Button, Divider, Grid } from 'semantic-ui-react';
 import ContentNFTs from './ContentNFTs';
 import Aux from '../_Aux';
 import Spinner from '../Spinner';
@@ -22,17 +22,18 @@ const Farming = () => {
     }
   });
 
-  function Pools() {
+  function Farming() {
     return (
       <div>
         <span style={{ display: 'flex' }} className="outter-DG-container">
           <span style={{ display: 'flex', flexDirection: 'column' }}>
             <h3 className="DG-h3">
-              Decentral Games Liquidity Mining
+              Decentral Games Farming
             </h3>
             <p>
-              Deposit your Liquidity Provider tokens to receive DG, the Decentral Games protocol governance token. 
-              To learn more about DG and the Decentral Games protocol, check out our protocol <a href="https://decentral-games-1.gitbook.io/" style={{ color: '#2085f4' }}>documentation</a>.
+              Users are able to farm DG in two ways, through gameplay and by providing liquidity. 
+              After depositing to a liquidity pool, users will recieve LP tokens they can then stake
+              in order to farm DG. Read more about DG rewards by visiting our <a href="https://decentral-games-1.gitbook.io/dg/governance-1" style={{ color: '#2085f4' }}>documentation</a>.
             </p>
           </span>
         </span>
@@ -55,12 +56,6 @@ const Farming = () => {
 
             </span>
 
-            <span style={{ display: 'flex', justifyContent: 'flex-end' }}>
-              <Button disabled className="DG-deposit-button">
-                DEPOSIT
-              </Button>
-            </span>
-
             <Divider />
 
             <span style={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -75,11 +70,16 @@ const Farming = () => {
               <p className="earned-amount"> 0 / week </p>
             </span>
 
-            <Divider className="DG-mobile-divider"/>
+            <Divider/>
 
-            <Button disabled className="DG-deposit-button-mobile">
-              DEPOSIT
-            </Button>
+            <span className="DG-button-span">
+              <Button disabled className="DG-deposit-button">
+                DEPOSIT
+              </Button>
+              <Button disabled className="DG-stake-button">
+                STAKE LP
+              </Button>
+            </span>
 
           </div>
 
@@ -100,12 +100,6 @@ const Farming = () => {
 
             </span>
 
-            <span style={{ display: 'flex', justifyContent: 'flex-end' }}>
-              <Button disabled className="DG-deposit-button">
-                DEPOSIT
-              </Button>
-            </span>
-
             <Divider />
 
             <span style={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -120,11 +114,17 @@ const Farming = () => {
               <p className="earned-amount"> 0 / week </p>
             </span>
 
-            <Divider className="DG-mobile-divider"/>
+            <Divider/>
 
-            <Button disabled className="DG-deposit-button-mobile">
-              DEPOSIT
-            </Button>
+            <span className="DG-button-span">
+              <Button disabled className="DG-deposit-button">
+                DEPOSIT
+              </Button>
+              <Button disabled className="DG-stake-button">
+                STAKE LP
+              </Button>
+            </span>
+
           </div>
         </div>
 
@@ -132,74 +132,29 @@ const Farming = () => {
     );
   }
 
-  function Liquidity() {
+  function Governance() {
     return (
       <div>
-        <span style={{ display: 'flex' }} className="outter-DG-container">
-          <span style={{ display: 'flex', flexDirection: 'column' }}>
-            <h3 className="DG-h3">
-              Liquidity Provider Rewards
-            </h3>
-            <p>
-              Liquidity providers earn a 0.3% fee on all trades proportional to their share of the pool. Fees are added to the pool, accrue in real time and can be claimed by withdrawing your liquidity.
-              Read more about providing liquidity <a href="https://decentral-games-1.gitbook.io/" style={{ color: '#2085f4' }}>here</a>.
-            </p>
+        <div className="outter-DG-container">
+          <span style={{ display: 'flex' }}>
+            <span style={{ display: 'flex', flexDirection: 'column' }}>
+              <h3 className="DG-h3">
+                Decentral Games Governance
+              </h3>
+              <p>
+                DG tokens represent voting shares in Decentral Games governance. Users can vote on each proposal yourself or delegate your votes to a third party.
+                Proposals can be found <a href="" style={{ color: '#2085f4' }}> here</a>. Read more about our governance model in our <a href="https://decentral-games-1.gitbook.io/dg/governance-1" style={{ color: '#2085f4' }}>documentation</a>.
+              </p>
+            </span>
           </span>
-        </span>
 
-        <span style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <p className="account-hover" style={{ marginTop: '30px' }}>
-            YOUR LIQUIDITY
-          </p>
-          <Button
-            disabled
-            className="DG-liquidity-button"
-            target="_blank"
-          >
-            ADD LIQUIDITY
-          </Button>
-        </span>
+          <div>
+            <Button disabled className="DG-stake-button-two">
+              STAKE DG
+            </Button>
+          </div>
 
-        <span style={{ display: 'flex', justifyContent: 'center' }} className="outter-DG-container">
-          <span style={{ display: 'flex', flexDirection: 'column' }}>
-            <h3 className="DG-h3" style={{ textAlign: 'center', paddingBottom: '0px' }}>
-              No Liquidity Found
-            </h3>
-          </span>
-        </span>
-      </div>
-    );
-  }
-
-  function Vote() {
-    return (
-      <div>
-        <span style={{ display: 'flex' }} className="outter-DG-container">
-          <span style={{ display: 'flex', flexDirection: 'column' }}>
-            <h3 className="DG-h3">
-              Decentral Games Governance
-            </h3>
-            <p>
-              DG tokens represent voting shares in Decentral Games governance. You can vote on each proposal yourself or delegate your votes to a third party.
-              Read more about our governance model <a href="https://decentral-games-1.gitbook.io/dg/governance-1" style={{ color: '#2085f4' }}>here</a>.
-            </p>
-          </span>
-        </span>
-
-        <p className="account-hover" style={{ marginTop: '30px' }}>
-          PROPOSALS
-        </p>
-
-        <span style={{ display: 'flex', justifyContent: 'center' }} className="outter-DG-container">
-          <span style={{ display: 'flex', flexDirection: 'column' }}>
-            <h3 className="DG-h3" style={{ textAlign: 'center' }}>
-              No proposals found
-            </h3>
-            <p style={{ textAlign: 'center' }}>
-              Proposals submitted by community members will appear here.
-            </p>
-          </span>
-        </span>
+        </div>
       </div>
     );
   }
@@ -210,40 +165,130 @@ const Farming = () => {
   function submenu() {
     return (
       <div className="account-other-tabs">
+
+        <div className="DG-top-container">
+          <div className="DG-column one top">
+            <span style={{ display: 'flex' }}>
+              <span className="avatar-picture">
+                <a
+                  href="https://play.decentraland.org/?OPEN_AVATAR_EDITOR&"
+                  target="_blank"
+                >
+                  <img
+                    className="avatar-picture"
+                    src={`https://events.decentraland.org/api/profile/${state.userInfo[1]}/face.png`}
+                    style={{
+                      width: '72px',
+                      display: 'flex',
+                      border: '1px solid rgb(227, 232, 238)',
+                      borderRadius: '100%',
+                      boxShadow: '0 0.75rem 1.5rem rgba(18, 38, 63, 0.03)',
+                    }}
+                  />
+                  <span className="avatar-edit"> edit </span>
+                </a>
+              </span>
+              <span style={{ display: 'flex', flexDirection: 'column' }}>
+                <p className="welcome-text"> Account Connected </p>
+                {state.userInfo[0] === null || state.userInfo[0] === '' ? (
+                  <p className="account-name">
+                    {state.userInfo[1].substr(0, 4) +
+                      '...' +
+                      state.userInfo[1].substr(-4)}
+                  </p>
+                ) : (
+                  <p className="account-name">{state.userInfo[0]}</p>
+                )}
+              </span>
+            </span>
+
+            <span style={{ display: 'flex', justifyContent: 'flex-end' }} className="account-authorize-span">
+              {state.userStatus === 6 ? (
+                <ButtonAuthorize />
+              ) : state.userStatus === 7 ? (
+                <ButtonEnable />
+              ) : (
+                <Button disabled className="account-connected-play-button">
+                  AUTHORIZE
+                </Button>
+              )}
+            </span>
+
+            <Divider className="DG-mobile-divider"/>
+
+            <span className="account-authorize-span-mobile">
+              {state.userStatus === 6 ? (
+                <ButtonAuthorize />
+              ) : state.userStatus === 7 ? (
+                <ButtonEnable />
+              ) : (
+                <Button disabled className="account-connected-play-button-mobile">
+                  AUTHORIZE
+                </Button>
+              )}
+            </span>
+          </div>
+
+          <div className="DG-column two">
+            <span style={{ display: 'flex' }}>
+              <span className="avatar-picture">
+                <img
+                  className="avatar-picture"
+                  src={Global.IMAGES.LOGO}
+                  style={{
+                    width: '72px',
+                    display: 'flex',
+                    border: '1px solid rgb(227, 232, 238)',
+                    borderRadius: '100%',
+                    boxShadow: '0 0.75rem 1.5rem rgba(18, 38, 63, 0.03)',
+                  }}
+                />
+              </span>
+              <span style={{ display: 'flex', flexDirection: 'column' }}>
+                <p className="welcome-text"> DG </p>
+                <p className="account-name"> 0 </p>
+              </span>
+            </span>
+
+            <span style={{ display: 'flex', justifyContent: 'flex-end' }} className="account-authorize-span">
+              <Button disabled className="account-connected-play-button">
+                REDEEM
+              </Button>
+            </span>
+
+            <Divider />
+
+            <span style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <p className="earned-text"> DG rewards </p>
+              <p className="earned-amount"> 0 </p>
+            </span>
+
+            <Divider className="DG-mobile-divider"/>
+
+            <span>
+              <Button disabled className="account-connected-play-button-mobile">
+                REDEEM
+              </Button>
+            </span>
+          </div>
+        </div>
         {
           (() => {
             if (DGstate == 0) return (
               <p className="account-other-p">
-                <b className="account-hover active">POOLS</b>{' '}
+                <b className="account-hover active">FARMING</b>{' '}
                 <abbr className="account-hover" onClick={() => setPage(1)}>
-                  LIQUIDITY
-                </abbr>
-                <abbr className="account-hover" onClick={() => setPage(2)}>
-                  VOTE
+                  GOVERNANCE
                 </abbr>
               </p>
-            )
-            else if (DGstate == 1) return (
-              <p className="account-other-p">
-                <abbr className="account-hover" onClick={() => setPage(0)}>
-                  POOLS
-                </abbr>{' '}
-                <b className="account-hover active">LIQUIDITY</b>
-                <abbr className="account-hover" onClick={() => setPage(2)}>
-                  VOTE
-                </abbr>{' '}
-              </p> 
             )
             else return (
               <p className="account-other-p">
                 <abbr className="account-hover" onClick={() => setPage(0)}>
-                  POOLS
+                  FARMING
                 </abbr>{' '}
-                <abbr className="account-hover" onClick={() => setPage(1)}>
-                  LIQUIDITY
-                </abbr>
-                <b className="account-hover active">VOTE</b>
-              </p>
+                <b className="account-hover active">GOVERNANCE</b>
+              </p> 
             )
           })()
         }
@@ -269,14 +314,11 @@ const Farming = () => {
           {
             (() => {
               if (DGstate == 0)
-                return Pools()
-              else if (DGstate == 1)
-                return Liquidity()
+                return Farming()
               else
-                return Vote()
+                return Governance()
             })()
           }
-
 
         </div>
       </div>}
