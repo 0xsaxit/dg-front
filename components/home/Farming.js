2e5c0abfe705274a4cb8ba93de8723c138fe51fe
0xsaxit <scott@flux.la>
2020-09-23 14:03:57 -0700

    /dg fix

--- components/home/Farming.js
+++ components/home/Farming.js
@@ -24,7 +24,95 @@ const Farming = () => {
     }
   });
 
-  function Farming() {
+  function gameplayFarming() {
+    return (
+      <div>
+        <div className="outter-DG-container">
+          <span style={{ display: 'flex' }}>
+            <span style={{ display: 'flex', flexDirection: 'column' }}>
+              <h3 className="DG-h3">
+                $DG Gameplay Farming
+              </h3>
+              <p>
+                You can farm $DG by playing games in our virtual casinos. Earn multipliers when sporting Decentral 
+                Games wearables and playing with friends. Read more about $DG rewards by visiting our <a href="https://decentral-games-1.gitbook.io/dg/governance-1" style={{ color: '#2085f4' }}>documentation</a>.
+              </p>
+            </span>
+          </span>
+
+          <div>
+            <Button disabled className="DG-stake-button-two">
+              REDEEM $DG
+            </Button>
+          </div>
+
+        </div>
+
+        <div className="DG-liquidity-container">
+          <div className="DG-column one">
+            <span style={{ display: 'flex' }}>
+              <img
+                src={Global.IMAGES.MANA_CIRCLE}
+                className="farming-logo"
+              />
+              <span className="farming-pool-span">
+                <p className="welcome-text"> Pool Name </p>
+                <p className="account-name">MANA-DG</p>
+              </span>
+
+            </span>
+
+            <Divider />
+
+            <span style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <p className="earned-text"> Total Spent </p>
+              <p className="earned-amount"> 0 </p>
+            </span>
+
+            <Divider />
+
+            <span style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <p className="earned-text"> Total Earned </p>
+              <p className="earned-amount"> 0 </p>
+            </span>
+
+          </div>
+
+          <div className="DG-column two">
+            <span style={{ display: 'flex' }}>
+              <img
+                src={Global.IMAGES.DAI_CIRCLE}
+                className="farming-logo"
+              />
+              <span className="farming-pool-span">
+                <p className="welcome-text"> Coin </p>
+                <p className="account-name">DAI</p>
+              </span>
+
+            </span>
+
+            <Divider />
+
+            <span style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <p className="earned-text"> Total Spent </p>
+              <p className="earned-amount"> 0 </p>
+            </span>
+
+            <Divider />
+
+            <span style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <p className="earned-text"> Total Earned </p>
+              <p className="earned-amount"> 0 </p>
+            </span>
+
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+
+  function liquidityFarming() {
     return (
       <div>
         <span style={{ display: 'flex' }} className="outter-DG-container">
@@ -33,9 +121,8 @@ const Farming = () => {
               $DG Liquidity Farming
             </h3>
             <p>
-              Users can farm DG in two ways, through gameplay and by providing liquidity.
               You can farm $DG by staking $MANA/$DG and $DAI/$DG balancer pool tokens here. 
-              Read more about DG rewards by visiting our  
+              Read more about $DG rewards by visiting our  
               <a href="https://decentral-games-1.gitbook.io/dg/governance-1" style={{ color: '#2085f4' }}> documentation</a>.
             </p>
           </span>
@@ -84,7 +171,10 @@ const Farming = () => {
 
             <span className="DG-button-span">
               <Button disabled className="DG-deposit-button">
-                DEPOSIT TO BALANCER POOL
+                DEPOSIT TO BALANCER
+              </Button>
+              <Button disabled className="DG-stake-button">
+                STAKE LP
               </Button>
             </span>
 
@@ -132,7 +222,10 @@ const Farming = () => {
 
             <span className="DG-button-span">
               <Button disabled className="DG-deposit-button">
-                DEPOSIT TO BALANCER POOL
+                DEPOSIT TO BALANCER
+              </Button>
+              <Button disabled className="DG-stake-button">
+                STAKE LP
               </Button>
             </span>
 
@@ -176,65 +269,40 @@ const Farming = () => {
   function submenu() {
     return (
       <div className="account-other-tabs">
-
-        <div className="DG-top-container">
-
-          <div className="DG-column zero">
-            <span style={{ display: 'flex' }}>
-              <span className="avatar-picture">
-                <img
-                  className="avatar-picture"
-                  src={Global.IMAGES.LOGO}
-                  style={{
-                    width: '72px',
-                    display: 'flex',
-                    border: '1px solid rgb(227, 232, 238)',
-                    borderRadius: '100%',
-                    boxShadow: '0 0.75rem 1.5rem rgba(18, 38, 63, 0.03)',
-                  }}
-                />
-              </span>
-              <span style={{ display: 'flex', flexDirection: 'column' }}>
-                <p className="welcome-text"> DG </p>
-                <p className="account-name"> 0 </p>
-              </span>
-            </span>
-
-            <span style={{ display: 'flex', justifyContent: 'flex-end' }} className="account-authorize-span">
-              <Button disabled className="account-connected-play-button">
-                REDEEM
-              </Button>
-            </span>
-
-            <Divider />
-
-            <span style={{ display: 'flex', justifyContent: 'space-between' }}>
-              <p className="earned-text"> Unclaimed $DG </p>
-              <p className="earned-amount"> 0 </p>
-            </span>
-
-            <Button disabled className="account-connected-play-button-mobile redeem">
-              REDEEM
-            </Button>
-          </div>
-        </div>
         {
           (() => {
             if (DGstate == 0) return (
               <p className="account-other-p">
-                <b className="account-hover active">LIQUIDITY FARMING</b>{' '}
+                <b className="account-hover active">GAMEPLAY</b>{' '}
                 <abbr className="account-hover" onClick={() => setPage(1)}>
+                  LIQUIDITY
+                </abbr>
+                <abbr className="account-hover" onClick={() => setPage(2)}>
                   GOVERNANCE
                 </abbr>
               </p>
             )
+            else if (DGstate == 1) return (
+              <p className="account-other-p">
+                <abbr className="account-hover" onClick={() => setPage(0)}>
+                  GAMEPLAY
+                </abbr>{' '}
+                <b className="account-hover active">LIQUIDITY</b>
+                <abbr className="account-hover" onClick={() => setPage(2)}>
+                  GOVERNANCE
+                </abbr>{' '}
+              </p> 
+            )
             else return (
               <p className="account-other-p">
                 <abbr className="account-hover" onClick={() => setPage(0)}>
-                  LIQUIDITY FARMING
+                  GAMEPLAY
                 </abbr>{' '}
+                <abbr className="account-hover" onClick={() => setPage(1)}>
+                  LIQUIDITY
+                </abbr>
                 <b className="account-hover active">GOVERNANCE</b>
-              </p> 
+              </p>
             )
           })()
         }
@@ -260,11 +328,14 @@ const Farming = () => {
           {
             (() => {
               if (DGstate == 0)
-                return Farming()
+                return gameplayFarming()
+              else if (DGstate == 1)
+                return liquidityFarming()
               else
                 return Governance()
             })()
           }
+
 
         </div>
       </div>}
