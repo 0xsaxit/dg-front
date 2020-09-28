6add180ac01c2261c0d08895ee767437d0a5de01
john doe <mockworld@gmail.com>
2020-09-28 20:30:53 +0700

    reverted {state.DGPoints}

--- components/home/Farming.js
+++ components/home/Farming.js
@@ -1,61 +1,57 @@
-import { useState, useEffect, useContext } from 'react'
-import { GlobalContext } from '../../store'
-import { Button, Divider, Radio  } from 'semantic-ui-react'
+import { useState, useEffect, useContext } from 'react';
+import { GlobalContext } from '../../store';
+import { Button, Divider, Radio } from 'semantic-ui-react';
 // import ButtonAuthorize from './ButtonAuthorize'
 // import ButtonEnable from './ButtonEnable'
 // import ContentNFTs from './ContentNFTs'
-import Aux from '../_Aux'
-import Spinner from '../Spinner'
-import Global from '../Constants'
+import Aux from '../_Aux';
+import Spinner from '../Spinner';
+import Global from '../Constants';
 
 const Farming = () => {
   // get user's unclaimed DG balance from the Context API store
-  const [state, dispatch] = useContext(GlobalContext)
+  const [state, dispatch] = useContext(GlobalContext);
 
   // define local variables
-  const [DGstate, setDGState] = useState(0)
-  const [isLoading, setIsLoading] = useState(true)
-  const [theme, setTheme] = useState('light')
-  const [themeStyle, setThemeStyle] = useState([])
+  const [DGstate, setDGState] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [theme, setTheme] = useState('light');
+  const [themeStyle, setThemeStyle] = useState([]);
 
   useEffect(() => {
     if (document.readyState === 'complete') {
-      setIsLoading(false)
+      setIsLoading(false);
     }
-  })
+  });
 
   const toggleTheme = () => {
     if (theme === 'light') {
       setTheme('dark');
-      setThemeStyle([
-        'dark',
-        'divider-dark'
-      ])
+      setThemeStyle(['dark', 'divider-dark']);
     } else {
       setTheme('light');
-      setThemeStyle([
-        '',
-        ''
-      ])
+      setThemeStyle(['', '']);
     }
-  }
+  };
 
   /////////////////////////////////////////////////////////////////////////////////////////
   /////////////////////////////////////////////////////////////////////////////////////////
   function gameplayFarming() {
     return (
       <Aux>
-
         <div className="DG-liquidity-container top">
           <div className="DG-column top">
             <span style={{ display: 'flex', flexDirection: 'column' }}>
-              <h3 className="DG-h3" id={themeStyle[0]}>$DG Gameplay Farming</h3>
+              <h3 className="DG-h3" id={themeStyle[0]}>
+                $DG Gameplay Farming
+              </h3>
               <p id={themeStyle[0]}>
-                You can farm $DG by playing games with $MANA or $DAI. 
-                Playing with two, three, or four players at the same table earns you 1.2x, 1.3x,
-                and 1.4x multipliers respectively, and repping a
-                Decentral Games wearable earns you an extra 10% $DG when playing. Refer any friends and get 
-                an additional 20% bonus on all $DG they farm. For more details see our
+                You can farm $DG by playing games with $MANA or $DAI. Playing
+                with two, three, or four players at the same table earns you
+                1.2x, 1.3x, and 1.4x multipliers respectively, and repping a
+                Decentral Games wearable earns you an extra 10% $DG when
+                playing. Refer any friends and get an additional 20% bonus on
+                all $DG they farm. For more details see our
                 <a
                   href="https://decentral-games-1.gitbook.io/dg/governance-1"
                   style={{ color: '#2085f4' }}
@@ -74,7 +70,7 @@ const Farming = () => {
               <img src={Global.IMAGES.LOGO} className="farming-logo" />
               <span className="farming-pool-span">
                 <p className="welcome-text"> Unclaimed DG</p>
-                <p className="account-name">0</p>
+                <p className="account-name">{state.DGPoints}</p>
               </span>
             </span>
 
@@ -91,30 +87,53 @@ const Farming = () => {
             <span style={{ display: 'flex' }}>
               <img src={Global.IMAGES.MANA_CIRCLE} className="farming-logo" />
               <span className="farming-pool-span">
-                <p className="welcome-text" id={themeStyle[0]}> Coin </p>
-                <p className="account-name" id={themeStyle[0]}>MANA</p>
+                <p className="welcome-text" id={themeStyle[0]}>
+                  {' '}
+                  Coin{' '}
+                </p>
+                <p className="account-name" id={themeStyle[0]}>
+                  MANA
+                </p>
               </span>
             </span>
 
-            <Divider className={themeStyle[1]}/>
+            <Divider className={themeStyle[1]} />
 
             <span style={{ display: 'flex', justifyContent: 'space-between' }}>
-              <p className="earned-text" id={themeStyle[0]}> Total Bet </p>
-              <p className="earned-amount" id={themeStyle[0]}> 0 </p>
+              <p className="earned-text" id={themeStyle[0]}>
+                {' '}
+                Total Bet{' '}
+              </p>
+              <p className="earned-amount" id={themeStyle[0]}>
+                {' '}
+                0{' '}
+              </p>
             </span>
 
-            <Divider className={themeStyle[1]}/>
+            <Divider className={themeStyle[1]} />
 
             <span style={{ display: 'flex', justifyContent: 'space-between' }}>
-              <p className="earned-text" id={themeStyle[0]}> $DG Farmed </p>
-              <p className="earned-amount" id={themeStyle[0]}> 0 </p>
+              <p className="earned-text" id={themeStyle[0]}>
+                {' '}
+                $DG Farmed{' '}
+              </p>
+              <p className="earned-amount" id={themeStyle[0]}>
+                {' '}
+                0{' '}
+              </p>
             </span>
 
-            <Divider className={themeStyle[1]}/>
+            <Divider className={themeStyle[1]} />
 
             <span style={{ display: 'flex', justifyContent: 'space-between' }}>
-              <p className="earned-text" id={themeStyle[0]}> Play-to-farm Rate </p>
-              <p className="earned-amount" id={themeStyle[0]}> 0 </p>
+              <p className="earned-text" id={themeStyle[0]}>
+                {' '}
+                Play-to-farm Rate{' '}
+              </p>
+              <p className="earned-amount" id={themeStyle[0]}>
+                {' '}
+                0{' '}
+              </p>
             </span>
           </div>
 
@@ -122,35 +141,58 @@ const Farming = () => {
             <span style={{ display: 'flex' }}>
               <img src={Global.IMAGES.DAI_CIRCLE} className="farming-logo" />
               <span className="farming-pool-span">
-                <p className="welcome-text" id={themeStyle[0]}> Coin </p>
-                <p className="account-name" id={themeStyle[0]}>DAI</p>
+                <p className="welcome-text" id={themeStyle[0]}>
+                  {' '}
+                  Coin{' '}
+                </p>
+                <p className="account-name" id={themeStyle[0]}>
+                  DAI
+                </p>
               </span>
             </span>
 
-            <Divider className={themeStyle[1]}/>
+            <Divider className={themeStyle[1]} />
 
             <span style={{ display: 'flex', justifyContent: 'space-between' }}>
-              <p className="earned-text" id={themeStyle[0]}> Total Bet </p>
-              <p className="earned-amount" id={themeStyle[0]}> 0 </p>
+              <p className="earned-text" id={themeStyle[0]}>
+                {' '}
+                Total Bet{' '}
+              </p>
+              <p className="earned-amount" id={themeStyle[0]}>
+                {' '}
+                0{' '}
+              </p>
             </span>
 
-            <Divider className={themeStyle[1]}/>
+            <Divider className={themeStyle[1]} />
 
             <span style={{ display: 'flex', justifyContent: 'space-between' }}>
-              <p className="earned-text" id={themeStyle[0]}> $DG Farmed </p>
-              <p className="earned-amount" id={themeStyle[0]}> 0 </p>
+              <p className="earned-text" id={themeStyle[0]}>
+                {' '}
+                $DG Farmed{' '}
+              </p>
+              <p className="earned-amount" id={themeStyle[0]}>
+                {' '}
+                0{' '}
+              </p>
             </span>
 
-            <Divider className={themeStyle[1]}/>
+            <Divider className={themeStyle[1]} />
 
             <span style={{ display: 'flex', justifyContent: 'space-between' }}>
-              <p className="earned-text" id={themeStyle[0]}> Play-to-farm Rate </p>
-              <p className="earned-amount" id={themeStyle[0]}> 0 </p>
+              <p className="earned-text" id={themeStyle[0]}>
+                {' '}
+                Play-to-farm Rate{' '}
+              </p>
+              <p className="earned-amount" id={themeStyle[0]}>
+                {' '}
+                0{' '}
+              </p>
             </span>
           </div>
         </div>
       </Aux>
-    )
+    );
   }
 
   /////////////////////////////////////////////////////////////////////////////////////////
@@ -163,9 +205,10 @@ const Farming = () => {
             <span style={{ display: 'flex', flexDirection: 'column' }}>
               <h3 className="DG-h3">$DG Liquidity Farming</h3>
               <p>
-                You can farm $DG by providing liquidity in 98% MANA • 2% DG and 98% DAI • 2% DG Balancer pools
-                and staking the balancer pool tokens on this dashboard.
-                 Read more about $DG liquidity farming rewards in our
+                You can farm $DG by providing liquidity in 98% MANA • 2% DG and
+                98% DAI • 2% DG Balancer pools and staking the balancer pool
+                tokens on this dashboard. Read more about $DG liquidity farming
+                rewards in our
                 <a
                   href="https://decentral-games-1.gitbook.io/dg/governance-1"
                   style={{ color: '#2085f4' }}
@@ -185,7 +228,7 @@ const Farming = () => {
               <img src={Global.IMAGES.LOGO} className="farming-logo" />
               <span className="farming-pool-span">
                 <p className="welcome-text"> Unclaimed DG</p>
-                <p className="account-name">0</p>
+                <p className="account-name">{state.DGPoints}</p>
               </span>
             </span>
 
@@ -285,7 +328,7 @@ const Farming = () => {
           </div>
         </div>
       </Aux>
-    )
+    );
   }
 
   /////////////////////////////////////////////////////////////////////////////////////////
@@ -300,7 +343,7 @@ const Farming = () => {
               <p>
                 Staked $DG tokens are used to vote in decentral.games
                 governance. To incentivize participation, for the first year
-                there will be farming rewards distributed to stakers in the $DG 
+                there will be farming rewards distributed to stakers in the $DG
                 governance contract. Proposals can be submitted and voted on{' '}
                 <a href="" style={{ color: '#2085f4' }}>
                   {' '}
@@ -325,7 +368,7 @@ const Farming = () => {
               <img src={Global.IMAGES.LOGO} className="farming-logo" />
               <span className="farming-pool-span">
                 <p className="welcome-text"> Unclaimed DG</p>
-                <p className="account-name">0</p>
+                <p className="account-name">{state.DGPoints}</p>
               </span>
             </span>
 
@@ -355,11 +398,9 @@ const Farming = () => {
               </Button>
             </span>
           </div>
-
         </div>
-
       </Aux>
-    )
+    );
   }
 
   /////////////////////////////////////////////////////////////////////////////////////////
@@ -372,46 +413,76 @@ const Farming = () => {
           if (DGstate == 0)
             return (
               <p className="account-other-p">
-                <b className="account-hover active" id={themeStyle[0]}>GAMEPLAY</b>{' '}
-                <abbr className="account-hover" id={themeStyle[0]} onClick={() => setPage(1)}>
+                <b className="account-hover active" id={themeStyle[0]}>
+                  GAMEPLAY
+                </b>{' '}
+                <abbr
+                  className="account-hover"
+                  id={themeStyle[0]}
+                  onClick={() => setPage(1)}
+                >
                   LIQUIDITY
                 </abbr>
-                <abbr className="account-hover" id={themeStyle[0]} onClick={() => setPage(2)}>
+                <abbr
+                  className="account-hover"
+                  id={themeStyle[0]}
+                  onClick={() => setPage(2)}
+                >
                   GOVERNANCE
                 </abbr>
               </p>
-            )
+            );
           else if (DGstate == 1)
             return (
               <p className="account-other-p">
-                <abbr className="account-hover" id={themeStyle[0]} onClick={() => setPage(0)}>
+                <abbr
+                  className="account-hover"
+                  id={themeStyle[0]}
+                  onClick={() => setPage(0)}
+                >
                   GAMEPLAY
                 </abbr>{' '}
-                <b className="account-hover active" id={themeStyle[0]}>LIQUIDITY</b>
-                <abbr className="account-hover" id={themeStyle[0]} onClick={() => setPage(2)}>
+                <b className="account-hover active" id={themeStyle[0]}>
+                  LIQUIDITY
+                </b>
+                <abbr
+                  className="account-hover"
+                  id={themeStyle[0]}
+                  onClick={() => setPage(2)}
+                >
                   GOVERNANCE
                 </abbr>{' '}
               </p>
-            )
+            );
           else
             return (
               <p className="account-other-p">
-                <abbr className="account-hover" id={themeStyle[0]} onClick={() => setPage(0)}>
+                <abbr
+                  className="account-hover"
+                  id={themeStyle[0]}
+                  onClick={() => setPage(0)}
+                >
                   GAMEPLAY
                 </abbr>{' '}
-                <abbr className="account-hover" id={themeStyle[0]} onClick={() => setPage(1)}>
+                <abbr
+                  className="account-hover"
+                  id={themeStyle[0]}
+                  onClick={() => setPage(1)}
+                >
                   LIQUIDITY
                 </abbr>
-                <b className="account-hover active" id={themeStyle[0]}>GOVERNANCE</b>
+                <b className="account-hover active" id={themeStyle[0]}>
+                  GOVERNANCE
+                </b>
               </p>
-            )
+            );
         })()}
       </div>
-    )
+    );
   }
 
   function setPage(number) {
-    setDGState(number)
+    setDGState(number);
   }
 
   return (
@@ -421,24 +492,26 @@ const Farming = () => {
       ) : (
         <div className="page-container">
           <div className="account-other-inner-container ">
-
             {submenu()}
             {/*<span style={{ display: 'flex', justifyContent: 'flex-end', width: '100%', marginTop: '-60px', marginBottom: '60px' }}>
               <Radio toggle onClick={toggleTheme} />
             </span>*/}
 
-            <Divider style={{ marginTop: '18px', paddingBottom: '21px' }} className={themeStyle[1]}/>
+            <Divider
+              style={{ marginTop: '18px', paddingBottom: '21px' }}
+              className={themeStyle[1]}
+            />
 
             {(() => {
-              if (DGstate == 0) return gameplayFarming()
-              else if (DGstate == 1) return liquidityFarming()
-              else return Governance()
+              if (DGstate == 0) return gameplayFarming();
+              else if (DGstate == 1) return liquidityFarming();
+              else return Governance();
             })()}
           </div>
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Farming
+export default Farming;
