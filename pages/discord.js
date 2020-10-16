bf8172b33305c78811a541c119ca10cb8823ab41
0xsaxit <scott@flux.la>
2020-10-15 18:40:07 -0700

    actual discord url fix

--- pages/discord.js
+++ pages/discord.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import Global from '../components/Constants';
+import React from 'react';  
+import Global from '../components/Constants'; 
 
-export default class extends React.Component {
-  static async getInitialProps({ res }) {
-    if (res) {
-      res.writeHead(301, {
-        Location: Global.DISCORD_URL,
-      });
-      res.end();
-    } else {
-      window.location = Global.DISCORD_URL;
-    }
-    return {};
-  }
+export default class extends React.Component {  
+  static async getInitialProps({ res }) { 
+    if (res) {  
+      res.writeHead(301, {  
+        Location: Global.CONSTANTS.DISCORD_URL, 
+      }); 
+      res.end();  
+    } else {  
+      window.location = Global.CONSTANTS.DISCORD_URL; 
+    } 
+    return {};  
+  } 
 }
