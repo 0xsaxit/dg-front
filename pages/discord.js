de414e25cc747547d002c406dfee1c960202e1ca
0xsaxit <scott@flux.la>
2020-10-15 18:37:54 -0700

    discord /url

---
+++ pages/discord.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Global from '../components/Constants';
+
+export default class extends React.Component {
+  static async getInitialProps({ res }) {
+    if (res) {
+      res.writeHead(301, {
+        Location: Global.DISCORD_URL,
+      });
+      res.end();
+    } else {
+      window.location = Global.DISCORD_URL;
+    }
+    return {};
+  }
+}
