91419216539837b446f2ef59cb96f4376c7bd7dd
0xsaxit <scott@flux.la>
2020-11-16 19:03:48 -0800

    remove unused npm packages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+// potential help with css loading issues
+const withCSS = require('@zeit/next-css')
+
+module.exports = withCSS({
+  cssLoaderOptions: {
+    url: false
+  }
+})
+
 // analyze the code bundles that are generated with Next.js
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
