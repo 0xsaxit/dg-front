0ee3f9050a2e2519c4870dc78fb330fbf38d4bd9
0xsaxit <scott@flux.la>
2020-11-16 19:04:33 -0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Head, Main, NextScript } from 'next/document';
-import { extractCritical } from '@emotion/server';
 import Global from '../components/Constants';
 
 export default class MyDocument extends Document {
@@ -7,24 +6,6 @@ export default class MyDocument extends Document {
   static async getServerSideProps(ctx) {
     const initialProps = await Document.getServerSideProps(ctx);
     return { ...initialProps };
-  }
-
-  static getInitialProps({ renderPage }) {
-    const page = renderPage();
-    const styles = extractCritical(page.html);
-    return {
-      ...page,
-      ...styles
-    }
-  }
-
-  constructor(props) {
-    super(props);
-    const { __NEXT_DATA__, ids } = props;
-
-    if (ids) {
-      __NEXT_DATA__.ids = ids;
-    }
   }
 
   render() {
