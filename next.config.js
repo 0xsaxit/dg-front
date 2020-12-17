c1e06cd4fbb4e584537c9eed8cfe2a8896c8b92d
UA DevOps <admin@uadevops.com>
2020-12-17 19:45:31 +0200

    testing env on server side and client side

--- next.config.js
+++ next.config.js
@@ -4,7 +4,15 @@ const withCSS = require('@zeit/next-css')
 module.exports = withCSS({
   cssLoaderOptions: {
     url: false
-  }
+  },
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 })
 
 // analyze the code bundles that are generated with Next.js
