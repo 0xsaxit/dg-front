b5cb5cd4eaa9838d4ba6c5cb3968a12c554f493c
john doe <mockworld@gmail.com>
2021-01-17 13:59:06 -0800

    removed bold font

--- pages/_document.js
+++ pages/_document.js
@@ -60,7 +60,7 @@ export default class MyDocument extends Document {
           /> */}
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap"
             rel="stylesheet"
           />
         </Head>
