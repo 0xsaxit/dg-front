d82792d6f97c3069f2cf679187e25545e9fa27cb
0xsaxit <scott@flux.la>
2021-01-19 12:24:35 -0800

    new treasury stuff

--- components/content/ContentGovernance.js
+++ components/content/ContentGovernance.js
@@ -1,12 +1,13 @@
-import { useEffect, useContext, useState } from 'react';
+import { useEffect, useContext, useState, React } from 'react';
 import { GlobalContext } from '../../store';
 import Web3 from 'web3';
-import { Button, Divider, Input, Loader } from 'semantic-ui-react';
+import { Button, Divider, Input, Loader, Popup, Icon } from 'semantic-ui-react';
 import Transactions from '../../common/Transactions';
 import Aux from '../_Aux';
 import Images from '../../common/Images';
 import Fetch from '../../common/Fetch';
 import Global from '../Constants';
+import { Chart } from 'react-charts';
 
 const ContentGovernance = (props) => {
   // get user's unclaimed DG balance from the Context API store
@@ -23,6 +24,10 @@ const ContentGovernance = (props) => {
   const [DGTokenContract, setDGTokenContract] = useState({});
   const [manaBalance, setManaBalance] = useState(0);
   const [daiBalance, setDaiBalance] = useState(0);
+  const [gameplayTreasury, setGameplayTreasury] = useState(0);
+  const [dgTreasury, setDgTreasury] = useState(0);
+  const [landTreasury, setLandTreasury] = useState(0);
+  const [statsUSD, setStatsUSD] = useState('');
   const [instances, setInstances] = useState(false);
 
   /////////////////////////////////////////////////////////////////////////////////////////
@@ -43,6 +48,68 @@ const ContentGovernance = (props) => {
   //   })();
   // }, []);
 
+  useEffect(() => {
+    (async function () {
+      // get treasury statistics
+      if (state.userStatus) {
+        let userAddress = window.web3.currentProvider.selectedAddress;
+        let response_3 = await Fetch.TREASURY_STATS(userAddress);
+        let json_3 = await response_3.json();
+        let usd = json_3.totalBalanceUSD;
+
+        setStatsUSD(usd);
+      }
+    })();
+  }, []); // statsUSD
+
+  let data;
+  let axes;
+
+  if (statsUSD.length > 0) {
+    statsUSD.map((stat) => (stat.primary = new Date(stat.primary)));
+
+    data = [
+      {
+        label: 'USD',
+        color: '#16c784',
+
+        data: [
+          ...new Array(
+            statsUSD.map((stat, i) => {
+              return {
+                primary: stat.primary,
+                secondary: stat.secondary,
+              };
+            })
+          )[0],
+        ],
+      },
+    ];
+
+    axes = [
+      { primary: true, type: 'time', position: 'bottom', show: false },
+      {
+        type: 'linear',
+        id: 'Second Metric',
+        min: 0,
+        position: 'left',
+        format: (d) => `$${d}`,
+        show: false
+      },
+    ];
+  } else {
+    data = [
+      {
+        label: 'Loading USD',
+        data: [],
+      },
+    ];
+
+    axes = [
+      { primary: true, type: 'utc', position: 'bottom', show: false },
+      { type: 'linear', position: 'left', show: false },
+    ];
+  }
 
   useEffect(() => {
     if (state.userStatus >= 4) {
@@ -63,27 +130,6 @@ const ContentGovernance = (props) => {
       fetchData();
     }
   }, [state.userStatus]);
-
-  useEffect(() => {
-    if (state.DGBalances.BALANCE_CHILD_MANA) {
-      (async () => {
-        const response = await Fetch.MANA_PRICE();
-        const json = await response.json();
-
-        const priceMANA = json.market_data.current_price.usd;
-        const balanceMANAUSD = state.DGBalances.BALANCE_CHILD_MANA * priceMANA;
-        const balanceMANA_CEO_USD = state.DGBalances.CEO_MANA * priceMANA;
-        const treasuryTotal =
-          Number(state.DGBalances.BALANCE_CHILD_DAI) + 
-          Number(balanceMANAUSD) +
-          Number(balanceMANA_CEO_USD) +
-          Number(state.DGBalances.CEO_DAI);
-        const treasuryTotalFormatted = props.formatPrice(treasuryTotal, 0);
-
-        setTreasuryTotal(treasuryTotalFormatted);
-      })();
-    }
-  }, [state.DGBalances.BALANCE_CHILD_MANA], [state.DGBalances.CEO_MANA]);
 
   useEffect(() => {
     if (state.DGBalances.BALANCE_CHILD_MANA) {
@@ -156,6 +202,40 @@ const ContentGovernance = (props) => {
     }
   }, [props.price, state.DGBalances.BALANCE_STAKING_GOVERNANCE]);
 
+  useEffect(() => {
+    if (state.DGBalances.BALANCE_CHILD_MANA) {
+      (async () => {
+        const response = await Fetch.MANA_PRICE();
+        const json = await response.json();
+
+        const priceMANA = json.market_data.current_price.usd;
+        const balanceMANAUSD = state.DGBalances.BALANCE_CHILD_MANA * priceMANA;
+        const balanceMANA_CEO_USD = state.DGBalances.CEO_MANA * priceMANA;
+        const gameplayTotal =
+          Number(state.DGBalances.BALANCE_CHILD_DAI) + 
+          Number(balanceMANAUSD) +
+          Number(balanceMANA_CEO_USD) +
+          Number(state.DGBalances.CEO_DAI);
+        const gameplayTotalFormatted = props.formatPrice(gameplayTotal, 0);
+
+        const dgTotal = Number(3000 * props.price);
+        const dgTotalFormatted = props.formatPrice(dgTotal, 0);
+
+        const landTotal = Number(400 * 1007.92);
+        const landTotalFormatted = props.formatPrice(landTotal, 0);
+
+        const totalTreasury = gameplayTotal + dgTotal + landTotal;
+        const treasuryTotalFormatted = props.formatPrice(totalTreasury, 0);
+
+        setGameplayTreasury(gameplayTotalFormatted);
+        setDgTreasury(dgTotalFormatted);
+        setLandTreasury(landTotalFormatted);
+        setTreasuryTotal(treasuryTotalFormatted);
+
+      })();
+    }
+  }, [state.DGBalances.BALANCE_CHILD_MANA], [state.DGBalances.CEO_MANA]);
+
   /////////////////////////////////////////////////////////////////////////////////////////
   /////////////////////////////////////////////////////////////////////////////////////////
   function handleChange(e) {
@@ -163,6 +243,12 @@ const ContentGovernance = (props) => {
   }
 
   function contentGovernance() {
+
+    const series = {
+      showPoints: false,
+      type: 'line',
+    };
+
     return (
       <Aux>
         <div className="DG-liquidity-container top">
@@ -170,9 +256,8 @@ const ContentGovernance = (props) => {
             <span style={{ display: 'flex', flexDirection: 'column' }}>
               <h3 className="DG-h3">$DG Governance</h3>
               <p>
-                Stake $DG tokens, govern the casino bankroll, and earn $DG
-                governance rewards. Proposal submission activates when the
-                treasury surpasses $500,000 USD. Read more about $DG governance
+                Stake $DG tokens, govern the $DG treasury, and earn $DG
+                governance rewards. Proposal submissions for casino profits activate when the gameplay treasury surpasses $500,000 USD. Users can submit proposals for LAND and $DG treasury usage immediately. Read more about $DG governance
                 in our{' '}
                 <a
                   href="https://www.decentral.games/blog/governance-staking-is-now-live-start-earning-dg-gov-rewards"
@@ -278,8 +363,199 @@ const ContentGovernance = (props) => {
             </span>
           </div>
 
-          <span className="DG-tablet-container">
-            <div className="DG-column one">
+          <span className="DG-tablet-container-gov">
+            <div
+              className="DG-column-treasury one"
+              style={{
+                position: 'relative',
+                height: '100%'
+              }}
+            >
+              <span style={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
+                <img
+                  src={Images.SNAPSHOT_ICON}
+                  className="farming-logo"
+                  id="snapshot"
+                  alt="Snapshot Governance Logo"
+                />
+                <span className="farming-pool-span" style={{ width: '48.5%' }}>
+                  <p className="welcome-text">total treasury</p>
+                  {treasuryTotal ? (
+                    <p className="account-name">${treasuryTotal}</p>
+                  ) : (
+                    <Loader
+                      active
+                      inline
+                      size="medium"
+                      style={{
+                        fontSize: '12px',
+                        marginTop: '12px',
+                        marginLeft: '15px',
+                      }}
+                    />
+                  )}
+                </span>
+
+                <span
+                  style={{
+                    width: '48.5%',
+                    maxWidth: '48.5%',
+                    height: '81px',
+                  }}
+                >
+                  <Chart
+                    data={data}
+                    axes={axes}
+                    series={series}
+                  />
+                </span>
+              </span>
+
+              <Divider />
+
+              <span
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  paddingTop: '12px',
+                  paddingBottom: '12px',
+                }}
+              >
+                <span style={{ display: 'flex' }}>
+                  <p className="earned-text">Gameplay Treasury</p>
+                  <Popup 
+                    className="dai-mana-popup"
+                    trigger={
+                      <Icon 
+                        className="dai-mana-icon" 
+                        name="info circle" 
+                        style={{ fontSize: '10px', marginLeft: '6px' }}
+                      />
+                    }
+                  >
+                    <div>
+                      <p className="earned-text">DAI: {daiBalance}</p>
+                      <p className="earned-text">MANA: {manaBalance}</p>
+                    </div>
+                  </Popup>
+                </span>
+                {gameplayTreasury ? (
+                  <p className="earned-amount">${gameplayTreasury}</p>
+                ) : (
+                  <Loader
+                    active
+                    inline
+                    size="small"
+                    style={{
+                      fontSize: '12px',
+                      marginTop: '1px',
+                      marginBottom: '2px',
+                    }}
+                  />
+                )}
+              </span>
+
+              <Divider />
+
+              <span
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  paddingTop: '12px',
+                  paddingBottom: '12px',
+                }}
+              >
+                <p className="earned-text">$DG Treasury</p>
+                {dgTreasury ? (
+                  <p className="earned-amount">${dgTreasury}</p>
+                ) : (
+                  <Loader
+                    active
+                    inline
+                    size="small"
+                    style={{
+                      fontSize: '12px',
+                      marginTop: '1px',
+                      marginBottom: '2px',
+                    }}
+                  />
+                )}
+              </span>
+
+              <Divider />
+
+              <span
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  paddingTop: '12px',
+                  paddingBottom: '12px',
+                }}
+              >
+                <span style={{ display: 'flex' }}>
+                  <p className="earned-text">LAND Treasury</p>
+                  <Popup 
+                    className="dai-mana-popup"
+                    trigger={
+                      <Icon 
+                        className="dai-mana-icon" 
+                        name="info circle" 
+                        style={{ fontSize: '10px', marginLeft: '6px' }}
+                      />
+                    }
+                  >
+                    <div>
+                      <p className="earned-text">calculated as 400 parcels times T30 avg LAND price </p>
+                    </div>
+                  </Popup>
+                </span>
+                {landTreasury ? (
+                  <p className="earned-amount">${landTreasury}</p>
+                ) : (
+                  <Loader
+                    active
+                    inline
+                    size="small"
+                    style={{
+                      fontSize: '12px',
+                      marginTop: '1px',
+                      marginBottom: '2px',
+                    }}
+                  />
+                )}
+              </span>
+
+              <Divider />
+
+              <span className="DG-button-span">
+                <Button
+                  href="https://discord.gg/VQ2ddfFBnu"
+                  target="_blank"
+                  className="DG-stake-button"
+                >
+                  DISCUSSION
+                </Button>
+                <Button
+                  href="https://snapshot.page/#/decentralgames.eth"
+                  target="_blank"
+                  className="DG-stake-button"
+                >
+                  VOTING
+                </Button>
+              </span>
+            </div>
+
+            <div 
+              className="DG-column-treasury two"
+              style={{
+                position: 'relative',
+                height: '100%',
+                maxHeight: '330px'
+              }}
+            >
               <span style={{ display: 'flex' }}>
                 <img
                   src={Images.DG_COIN_LOGO}
@@ -456,130 +732,9 @@ const ContentGovernance = (props) => {
               </span>
             </div>
 
-            <div
-              className="DG-column two"
-              style={{
-                position: 'relative',
-                height: '100%',
-                maxHeight: '258px',
-              }}
-            >
-              <span style={{ display: 'flex' }}>
-                <img
-                  src={Images.SNAPSHOT_ICON}
-                  className="farming-logo"
-                  id="snapshot"
-                  alt="Snapshot Governance Logo"
-                />
-                <span className="farming-pool-span">
-                  <p className="welcome-text">treasury balance</p>
-                  {treasuryTotal ? (
-                    <p className="account-name">${treasuryTotal}</p>
-                  ) : (
-                    <Loader
-                      active
-                      inline
-                      size="medium"
-                      style={{
-                        fontSize: '12px',
-                        marginTop: '12px',
-                        marginLeft: '15px',
-                      }}
-                    />
-                  )}
-                </span>
-              </span>
-
-              <Divider />
-
-              <div style={{ display: 'flex' }}>
-                <span className="gameplay-left-column">
-                  <span
-                    style={{
-                      display: 'flex',
-                      flexDirection: 'column',
-                      alignItems: 'center',
-                      paddingBottom: '17px',
-                    }}
-                  >
-                    <p className="earned-text">MANA</p>
-                    {state.DGBalances.BALANCE_CHILD_MANA ? (
-                      <p className="earned-amount">
-                        {manaBalance}
-                      </p>
-                    ) : (
-                      <Loader
-                        active
-                        inline
-                        size="small"
-                        style={{
-                          fontSize: '12px',
-                          marginTop: '5px',
-                          marginLeft: '-1px',
-                          marginBottom: '-2px',
-                        }}
-                      />
-                    )}
-                  </span>
-                </span>
-
-                <span
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    width: '50%',
-                  }}
-                >
-                  <span
-                    style={{
-                      display: 'flex',
-                      flexDirection: 'column',
-                      alignItems: 'center',
-                    }}
-                  >
-                    <p className="earned-text">dai</p>
-                    {state.DGBalances.BALANCE_CHILD_DAI ? (
-                      <p className="earned-amount">
-                        {daiBalance}
-                      </p>
-                    ) : (
-                      <Loader
-                        active
-                        inline
-                        size="small"
-                        style={{
-                          fontSize: '12px',
-                          marginTop: '5px',
-                          marginLeft: '-1px',
-                          marginBottom: '-2px',
-                        }}
-                      />
-                    )}
-                  </span>
-                </span>
-              </div>
-
-              <Divider />
-
-              <span className="DG-button-span">
-                <Button
-                  href="https://discord.gg/VQ2ddfFBnu"
-                  target="_blank"
-                  className="DG-stake-button"
-                >
-                  DISCUSSION
-                </Button>
-                <Button
-                  href="https://snapshot.page/#/decentralgames.eth"
-                  target="_blank"
-                  className="DG-stake-button"
-                >
-                  VOTING
-                </Button>
-              </span>
-            </div>
           </span>
         </div>
+
       </Aux>
     );
   }
