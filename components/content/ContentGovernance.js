e80fea21801350b324487cc2a281030ed3530714
0xsaxit <41426816+0xsaxit@users.noreply.github.com>
2021-01-20 15:40:57 -0800

    update land treasury

--- components/content/ContentGovernance.js
+++ components/content/ContentGovernance.js
@@ -222,7 +222,7 @@ const ContentGovernance = (props) => {
         const dgTotal = Number(3000 * props.price);
         const dgTotalFormatted = props.formatPrice(dgTotal, 0);
 
-        const landTotal = Number(400 * 1211.46);
+        const landTotal = Number(403 * 1288.25);
         const landTotalFormatted = props.formatPrice(landTotal, 0);
 
         const nftPrice = 5000 * priceMANA;
