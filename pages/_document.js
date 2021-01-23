aad3332b7f240fbc595d6d24f37a34481af6df49
john doe <mockworld@gmail.com>
2021-01-22 20:59:18 -0800

    added segment snippet

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,95 @@ export default class MyDocument extends Document {
     return { ...initialProps };
   }
 
+  segmentSnippet() {
+    if (typeof window !== 'undefined') {
+      // create a queue, but don't obliterate an existing one
+      var analytics = (window.analytics = window.analytics || []);
+
+      // if the real analytics.js is already on the page return
+      if (analytics.initialize) return;
+
+      // if the snippet was invoked already show an error
+      if (analytics.invoked) {
+        if (window.console && console.error) {
+          console.error('Segment snippet included twice.');
+        }
+        return;
+      }
+
+      // invoked flag, to make sure the snippet is never invoked twice
+      analytics.invoked = true;
+
+      // list of the methods in Analytics.js to stub
+      analytics.methods = [
+        'trackSubmit',
+        'trackClick',
+        'trackLink',
+        'trackForm',
+        'pageview',
+        'identify',
+        'reset',
+        'group',
+        'track',
+        'ready',
+        'alias',
+        'debug',
+        'page',
+        'once',
+        'off',
+        'on',
+        'addSourceMiddleware',
+        'addIntegrationMiddleware',
+        'setAnonymousId',
+        'addDestinationMiddleware',
+      ];
+
+      // define a factory to create stubs. These are placeholders for methods in Analytics.js so that you never have to wait
+      // for it to load to actually record data. The `method` is stored as the first argument, so we can replay the data
+      analytics.factory = function (method) {
+        return function () {
+          var args = Array.prototype.slice.call(arguments);
+          args.unshift(method);
+          analytics.push(args);
+          return analytics;
+        };
+      };
+
+      // for each of our methods, generate a queueing stub
+      for (var i = 0; i < analytics.methods.length; i++) {
+        var key = analytics.methods[i];
+        analytics[key] = analytics.factory(key);
+      }
+
+      // define a method to load Analytics.js from our CDN, and that will be sure to only ever load it once
+      analytics.load = function (key, options) {
+        // create an async script element based on your key
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.async = true;
+        script.src =
+          'https://cdn.segment.com/analytics.js/v1/' +
+          key +
+          '/analytics.min.js';
+
+        // insert our script next to the first script element.
+        var first = document.getElementsByTagName('script')[0];
+        first.parentNode.insertBefore(script, first);
+        analytics._loadOptions = options;
+      };
+
+      // add a version to keep track of what's in the wild.
+      analytics.SNIPPET_VERSION = '4.1.0';
+
+      // load Analytics.js with your key, which will automatically load the tools you've enabled for your account
+      analytics.load(Global.SEGMENT_WRITE_KEY);
+
+      // make the first page call to load the integrations
+      // if you'd like to manually name or tag the page, edit or move this call however you'd like
+      analytics.page();
+    }
+  }
+
   render() {
     return (
       <Html lang="en">
@@ -58,6 +147,8 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             disabled
           />
+
+          <script dangerouslySetInnerHTML={{ __html: this.segmentSnippet() }} />
         </Head>
 
         <body>
