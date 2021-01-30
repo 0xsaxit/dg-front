7bf9c818a33da6d8b250d1c471207cda49ea8443
john doe <mockworld@gmail.com>
2021-01-29 16:26:04 -0800

    segment updates

--- components/home/Footer.js
+++ components/home/Footer.js
@@ -1,4 +1,4 @@
-import { useEffect, useContext } from 'react';
+import { useEffect, useContext, useState } from 'react';
 import { GlobalContext } from '../../store';
 import { useRouter } from 'next/router';
 import { Menu, Icon } from 'semantic-ui-react';
@@ -9,6 +9,10 @@ const Footer = () => {
   const [state, dispatch] = useContext(GlobalContext);
 
   // define local variables
+  const [DGStaking, setDGStaking] = useState(0);
+  const [DGMainchain, setDGMainchain] = useState(0);
+  const [complete, setComplete] = useState(false);
+
   const router = useRouter();
 
   /////////////////////////////////////////////////////////////////////////////////////////
@@ -24,6 +28,54 @@ const Footer = () => {
       address: state.userAddress,
     });
   }, [router.pathname]);
+
+  // get DG staking and DG mainchan amounts
+  useEffect(() => {
+    if (
+      state.stakingBalances.BALANCE_USER_GOVERNANCE &&
+      state.DGBalances.BALANCE_ROOT_DG
+    ) {
+      const DGStaking = formatPrice(
+        state.stakingBalances.BALANCE_USER_GOVERNANCE,
+        3
+      );
+      const DGMainchain = formatPrice(state.DGBalances.BALANCE_ROOT_DG, 3);
+
+      setDGStaking(DGStaking);
+      setDGMainchain(DGMainchain);
+
+      setComplete(true);
+    }
+  }, [
+    state.stakingBalances.BALANCE_USER_GOVERNANCE,
+    state.DGBalances.BALANCE_ROOT_DG,
+  ]);
+
+  function formatPrice(balance, units) {
+    const balanceAdjusted = Number(balance)
+      .toFixed(units)
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+    return balanceAdjusted;
+  }
+
+  // send user identity data to Segment analytics
+  useEffect(() => {
+    if (complete && state.userInfo.length) {
+      analytics.identify(state.userAddress, {
+        name: state.userInfo[0],
+        userStatus: state.userStatus,
+        email: state.userInfo[4],
+        DG: {
+          stakedGovernance: DGStaking,
+          mainchainWallet: DGMainchain,
+        },
+        PLAY: { slots: [], roulette: [], blackjack: [], poker: [] },
+        DAI: { slots: [], roulette: [], blackjack: [], poker: [] },
+        MANA: { slots: [], roulette: [], blackjack: [], poker: [] },
+      });
+    }
+  }, [complete, state.userInfo]);
 
   return (
     <span className="footer-container">
--- store/UserInfo.js
+++ store/UserInfo.js
@@ -6,77 +6,28 @@ function UserInfo() {
   // dispatch user's information to the Context API store
   const [state, dispatch] = useContext(GlobalContext);
 
-  // define local variables
-  const [DGStaking, setDGStaking] = useState(0);
-  const [DGMainchain, setDGMainchain] = useState(0);
-  const [complete, setComplete] = useState(false);
-
-  /////////////////////////////////////////////////////////////////////////////////////////
-  /////////////////////////////////////////////////////////////////////////////////////////
+  // get user's play name, wallet address, avatar balance, and email address
   useEffect(() => {
-    if (
-      state.stakingBalances.BALANCE_USER_GOVERNANCE &&
-      state.DGBalances.BALANCE_ROOT_DG
-    ) {
-      const DGStaking = formatPrice(
-        state.stakingBalances.BALANCE_USER_GOVERNANCE,
-        3
-      );
-      const DGMainchain = formatPrice(state.DGBalances.BALANCE_ROOT_DG, 3);
-
-      setDGStaking(DGStaking);
-      setDGMainchain(DGMainchain);
-
-      setComplete(true);
-    }
-  }, [
-    state.stakingBalances.BALANCE_USER_GOVERNANCE,
-    state.DGBalances.BALANCE_ROOT_DG,
-  ]);
-
-  function formatPrice(balance, units) {
-    const balanceAdjusted = Number(balance)
-      .toFixed(units)
-      .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-
-    return balanceAdjusted;
-  }
-
-  useEffect(() => {
-    if (complete) {
-      // get user's play name, wallet address, and avatar balance
+    if (state.userAddress) {
       (async function () {
-        let responseInfo = await Fetch.PLAYER_INFO(state.userAddress);
-        let json = await responseInfo.json();
+        const responseInfo = await Fetch.PLAYER_INFO(state.userAddress);
+        const json = await responseInfo.json();
 
         const name = json.avatarName;
         const address = json.address;
         const balance = json.playBalance.toLocaleString();
         const count = json.callCount;
+        const email = '';
 
-        const response = [name, address, balance, count];
+        const response = [name, address, balance, count, email];
 
         dispatch({
           type: 'user_info',
           data: response,
         });
-
-        // send identity data to Segment analytics
-        analytics.identify(address, {
-          name: name,
-          userStatus: state.userStatus,
-          email: '',
-          DG: {
-            stakedGovernance: DGStaking,
-            mainchainWallet: DGMainchain,
-          },
-          PLAY: { slots: [], roulette: [], blackjack: [], poker: [] },
-          DAI: { slots: [], roulette: [], blackjack: [], poker: [] },
-          MANA: { slots: [], roulette: [], blackjack: [], poker: [] },
-        });
       })();
     }
-  }, [complete]);
+  }, [state.userAddress]);
 
   return null;
 }
