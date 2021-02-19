47b381d1b04dca060bf4934b9fe1b1b065234e13
b4u5 <milesanthony@ucla.edu>
2021-02-19 13:31:36 +0000

    update mining rates

--- components/content/ContentMining.js
+++ components/content/ContentMining.js
@@ -246,7 +246,7 @@ const ContentMining = (props) => {
                     }}
                   >
                     <p className="earned-text">Roulette Rate / 1 DG</p>
-                    <p className="earned-amount">31.3K MANA</p>
+                    <p className="earned-amount">46K MANA</p>
                   </span>
                 </span>
 
@@ -266,7 +266,7 @@ const ContentMining = (props) => {
                     }}
                   >
                     <p className="earned-text">Blackjack Rate / 1 DG</p>
-                    <p className="earned-amount">106K MANA</p>
+                    <p className="earned-amount">156K MANA</p>
                   </span>
                 </span>
               </div>
@@ -310,7 +310,7 @@ const ContentMining = (props) => {
                     }}
                   >
                     <p className="earned-text">Roulette Rate / 1 DG</p>
-                    <p className="earned-amount">11K DAI</p>
+                    <p className="earned-amount">14.3K DAI</p>
                   </span>
                 </span>
 
@@ -329,7 +329,7 @@ const ContentMining = (props) => {
                     }}
                   >
                     <p className="earned-text">Blackjack Rate / 1 DG</p>
-                    <p className="earned-amount">37K DAI</p>
+                    <p className="earned-amount">48.2K DAI</p>
                   </span>
                 </span>
               </div>
