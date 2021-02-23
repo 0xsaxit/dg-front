dafd15b9771d65bd02b4b1ca59dc5a51383df501
0xsaxit <41426816+0xsaxit@users.noreply.github.com>
2021-02-22 17:32:01 -0800

    event page update

--- components/home/EventData.js
+++ components/home/EventData.js
@@ -51,88 +51,170 @@ const EventData = () => {
   /////////////////////////////////////////////////////////////////////////////////////////
   function contentEvents() {
     return (
-      <Grid>
-        {events.map((event, i) => (
-          <Grid.Column
-            computer={5}
-            tablet={8}
-            mobile={16}
-            className="leaderboard-column"
-            id='nine-nine-one'
-            key={i}
-          >
-            <a href={event.url} className="my-nft-container">
-              <div>
-                <span
-                  style={{ display: 'flex', justifyContent: 'center' }}
-                  className="event-image"
+      <span>
+
+        <div className="account-other-tabs" style={{ paddingTop: '15px' }}>
+          <div style={{ marginLeft: '0px' }}>
+            <span className="account-other-p" style={{ display: 'flex' }}>
+              <span className="account-hover active">
+                <b>FEATURED EVENT</b>
+              </span>
+            </span>
+          </div>
+        </div>
+
+        <Divider className="tab-divider" style={{ marginBottom: '35px' }} />
+
+        <div>
+          <a href={events[0].url} className="my-nft-container">
+            <span className="featured-event-grid">
+              <Image src={events[0].image} className="event-pic featured" />
+
+              <div className="post-info featured">
+                <h3
+                  className="nft-other-h3 featured"
+                  style={{
+                    paddingBottom: '9px',
+                    paddingTop: '9px'
+                  }}
                 >
-                  <Image
-                    src={event.image}
-                    className="event-pic"
-                    style={{ borderRadius: '4px 4px 0px 0px' }}
-                  />
-                </span>
-                <div className="nft-description">
-                  <h3 className="nft-other-h3">{event.name}</h3>
-                  <span style={{ display: 'flex', justifyContent: 'center' }}>
-                    <p className="nfts-info"> going: {event.total_attendees}</p>
-                    <p className="nfts-info-2">{getFormattedDate(event.next_start_at)}</p>
-                  </span>
-
-                  <Divider
-                    style={{
-                      margin: '10px 0px 15px 0px',
-                      width: 'calc(100% + 60px)',
-                      marginLeft: '-30px',
-                    }}
-                  />
-
-                  <p
-                    className="nft-other-p"
-                    style={{
-                      marginTop: '-12px',
-                      paddingTop: '15px',
-                      textAlign: 'center',
-                    }}
-                  >
-                    {getSentences(event.description)}
+                  {events[0].name}
+                </h3>
+                <span className="featured-info">
+                  <p className="nfts-info">
+                    {events[0].next_start_at}
                   </p>
+                </span>
 
-                  <span
-                    style={{ display: 'flex', justifyContent: 'space-between' }}
+                <Divider className="events-featured-divider" style={{ margin: '10px 0px 15px 0px' }}/>
+
+                <p
+                  style={{
+                    lineHeight: '1.3',
+                    paddingTop: '3px'
+                  }}
+                  className="events-featured-p"
+                >
+                  {getSentences(events[0].description)}
+                </p>
+                <span
+                  style={{ display: 'flex', justifyContent: 'space-between', paddingTop: '15px' }}
+                >
+                  <Button
+                    color="blue"
+                    className="nft-button"
+                    target="_blank"
+                    href={events[0].url}
                   >
-                    <Button
-                      color="blue"
-                      className="nft-button"
-                      target="_blank"
-                      href={event.url}
-                    >
-                      HOP IN
-                    </Button>
-                    <Button
-                      className="nft-read-button"
-                      target="_blank"
-                      href={`https://events.decentraland.org/en/?event=${event.id}`}
-                    >
-                      RSVP
-                    </Button>
-                  </span>
-                </div>
+                    HOP IN
+                  </Button>
+                  <Button
+                    className="nft-read-button"
+                    target="_blank"
+                    href={`https://events.decentraland.org/en/?event=${events[0].id}`}
+                  >
+                    RSVP
+                  </Button>
+                </span>
               </div>
-            </a>
-          </Grid.Column>
-        ))}
-      </Grid>
+            </span>
+          </a>
+        </div>
+
+        <div className="account-other-tabs" style={{ marginTop: '30px' }}>
+          <div style={{ marginLeft: '0px' }}>
+            <span className="account-other-p" style={{ display: 'flex' }}>
+              <span className="account-hover active">
+                <b>ALL EVENTS</b>
+              </span>
+            </span>
+          </div>
+        </div>
+
+        <Divider className="tab-divider" />
+
+        <Grid>
+          {events.slice(1).map((event, i) => (
+            <Grid.Column
+              computer={5}
+              tablet={8}
+              mobile={16}
+              className="leaderboard-column"
+              id='nine-nine-one'
+              key={i}
+            >
+              <a href={event.url} className="my-nft-container">
+                <div>
+                  <span
+                    style={{ display: 'flex', justifyContent: 'center' }}
+                    className="event-image"
+                  >
+                    <Image
+                      src={event.image}
+                      className="event-pic"
+                      style={{ borderRadius: '4px 4px 0px 0px' }}
+                    />
+                  </span>
+                  <div className="nft-description">
+                    <h3 className="nft-other-h3">{event.name}</h3>
+                    <span style={{ display: 'flex', justifyContent: 'center' }}>
+                      <p className="nfts-info">{event.next_start_at}</p>
+                    </span>
+
+                    <Divider
+                      style={{
+                        margin: '10px 0px 15px 0px',
+                        width: 'calc(100% + 60px)',
+                        marginLeft: '-30px',
+                      }}
+                    />
+
+                    <p
+                      className="nft-other-p"
+                      style={{
+                        marginTop: '-12px',
+                        paddingTop: '15px',
+                        textAlign: 'center',
+                      }}
+                    >
+                      {getSentences(event.description)}
+                    </p>
+
+                    <span
+                      style={{ display: 'flex', justifyContent: 'space-between' }}
+                    >
+                      <Button
+                        color="blue"
+                        className="nft-button"
+                        target="_blank"
+                        href={event.url}
+                      >
+                        HOP IN
+                      </Button>
+                      <Button
+                        className="nft-read-button"
+                        target="_blank"
+                        href={`https://events.decentraland.org/en/?event=${event.id}`}
+                      >
+                        RSVP
+                      </Button>
+                    </span>
+                  </div>
+                </div>
+              </a>
+            </Grid.Column>
+          ))}
+        </Grid>
+      </span>
     );
   }
 
   return (
-    <div className="main-container" style={{ padding: '96px 0px 0px 0px' }}>
+    <div className="main-container">
       {loading === true ? (
         <Spinner background={1} />
       ) : (
-        <div className="page-container" style={{ padding: '0px 30px 120px 30px' }}>
+        <div className="page-container" id="event-container">
           {contentEvents()}
         </div>
       )}
