d7416339d0412982e05e02c1f58276714438eb18
john doe <mockworld@gmail.com>
2021-03-07 17:01:43 -0800

    added website intel

--- components/content/ContentIntelligence.js
+++ components/content/ContentIntelligence.js
@@ -1,38 +1,15 @@
-// import { Image, Button, Divider } from 'semantic-ui-react';
-// import { useState, useEffect } from 'react';
-
 const ContentIntelligence = (props) => {
   /////////////////////////////////////////////////////////////////////////////////////////
   /////////////////////////////////////////////////////////////////////////////////////////
   function contentServer() {
     return (
       <div className="outter-games-container">
-        {/* <iframe
-          src="https://intel.decentral.games/embed/dashboard/11"
-          frameBorder={0}
-          width={800}
-          height={600}
-          allowTransparency
-        /> */}
-
-        {/* <iframe
-          src="https://intel.decentral.games/public/dashboard/50db5987-28dd-4d2e-a47b-e33f78b2d267"
-          frameborder="0"
-          width="100%"
-          height="100%"
-          overflow="scroll"
-          scrolling="no"
-          allowtransparency
-        ></iframe> */}
-
         <iframe
           src="https://intel.decentral.games/public/dashboard/50db5987-28dd-4d2e-a47b-e33f78b2d267"
           frameborder="0"
           allowTransparency
           overflow="hidden"
           height="2500px"
-          // scrolling="no"
-          // position="absolute"
           width="1340px"
         />
       </div>
@@ -42,13 +19,30 @@ const ContentIntelligence = (props) => {
   /////////////////////////////////////////////////////////////////////////////////////////
   /////////////////////////////////////////////////////////////////////////////////////////
   function contentWebsite() {
-    return <div className="outter-games-container">website intel...</div>;
+    return (
+      <div className="outter-games-container">
+        <div className="outter-games-container">
+          <iframe
+            src="https://intel.decentral.games/public/dashboard/c8c993b4-f0ef-45af-b702-e8ddfbff1b3f"
+            frameborder="0"
+            allowTransparency
+            overflow="hidden"
+            height="1500px"
+            width="1340px"
+          />
+        </div>
+      </div>
+    );
   }
 
   /////////////////////////////////////////////////////////////////////////////////////////
   /////////////////////////////////////////////////////////////////////////////////////////
   function contentDecentraland() {
-    return <div className="outter-games-container">decentraland intel...</div>;
+    return (
+      <div className="outter-games-container">
+        Decentraland intel coming soon...
+      </div>
+    );
   }
 
   if (props.dashboard === 'server') {
