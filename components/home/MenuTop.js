812c18ab14dec533d14ef1e44e73b89e41c65da3
0xsaxit <41426816+0xsaxit@users.noreply.github.com>
2021-06-14 13:48:44 -0700

    Update MenuTop.js

--- components/home/MenuTop.js
+++ components/home/MenuTop.js
@@ -8,6 +8,8 @@ import MessageBar from './MessageBar';
 import ButtonConnect from '../button/ButtonConnect';
 import Fetch from '../../common/Fetch';
 import Global from '../Constants';
+import ModalPopup from 'components/modal/ModalPopup';
+
 
 const MenuTop = (props) => {
   // get token balances from the Context API store
@@ -351,136 +353,7 @@ const MenuTop = (props) => {
       return (
         <span className="right-menu-items">
           <ModalInfo />
-
-          <div>
-            <Popup
-              pinned
-              on="click"
-              position="bottom right"
-              className="account-popup"
-              trigger={
-                <Button className="account-button">
-                  <Icon className="account-icon" name="user circle outline" />
-                  My Account
-                </Button>
-              }
-            >
-              <span>
-                <span style={{ display: 'flex' }}>
-                  <img
-                    className="avatar-picture-home"
-                    src={`https://events.decentraland.org/api/profile/${state.userAddress}/face.png`}
-                  />
-                  <span style={{ display: 'flex', flexDirection: 'column' }}>
-                    {state.userInfo.name === null ||
-                    state.userInfo.name === '' ? (
-                      <h4 style={{ paddingLeft: '8px', marginTop: '-4px' }}>
-                        Unnamed
-                      </h4>
-                    ) : (
-                      <h4 style={{ paddingLeft: '8px', marginTop: '-4px' }}>
-                        {state.userInfo.name}
-                      </h4>
-                    )}
-                    <span
-                      className="account-copy"
-                      style={{ display: 'flex' }}
-                      onClick={() => onCopy()}
-                    >
-                      <p className="account-address">
-                        {state.userAddress.substr(0, 8) +
-                          '...' +
-                          state.userAddress.substr(-8)}
-                        <Icon
-                          name="clone outline"
-                          style={{
-                            color: 'rgba(225, 255, 255, 0.5)',
-                            fontSize: '16px',
-                            padding: '0px 0px 0px 8px',
-                          }}
-                        />
-                      </p>
-                    </span>
-                  </span>
-                </span>
-                <span style={{ display: 'flex', flexDirection: 'column' }}>
-                  <Link href="/account">
-                    <Button className="casino-balance-button">
-                      <p className="casino-balance-text"> Casino Balance </p>
-
-                      <span
-                        style={{ display: 'flex', justifyContent: 'flex-end' }}
-                      >
-                        <p className="casino-balance-text two">
-                          ${casinoBalance}
-                        </p>
-                        <Icon
-                          className="arrow right"
-                          style={{ paddingLeft: '8px', paddingTop: '5px' }}
-                        />
-                      </span>
-                    </Button>
-                  </Link>
-
-                  <span
-                    style={{
-                      display: 'flex',
-                      justifyContent: 'space-between',
-                      marginTop: '12px',
-                    }}
-                  >
-                    <Link href="/account">
-                      <Button className="account-deposit-button">
-                        Deposit
-                      </Button>
-                    </Link>
-                    <Link href="/account">
-                      <Button className="account-withdraw-button">
-                        Withdraw
-                      </Button>
-                    </Link>
-                  </span>
-
-                  <Link href="/account">
-                    <p
-                      className="account-dropdown-item"
-                      style={{ marginTop: '8px' }}
-                    >
-                      My Account
-                    </p>
-                  </Link>
-                  <Link href="/account/nfts">
-                    <p className="account-dropdown-item">My NFTs</p>
-                  </Link>
-                  <Link href="/account/poaps">
-                    <p className="account-dropdown-item">My POAPs</p>
-                  </Link>
-                  <Link href="/account/play">
-                    <p className="account-dropdown-item">Gameplay History</p>
-                  </Link>
-                  <Link href="/account/history">
-                    <p className="account-dropdown-item">Transactions</p>
-                  </Link>
-                  <Link href="/account/referrals">
-                    <p className="account-dropdown-item">Referrals</p>
-                  </Link>
-                  <Button
-                    className="buy-dg-button"
-                    href={`https://app.uniswap.org/#/swap?outputCurrency=${Global.ADDRESSES.ROOT_TOKEN_ADDRESS_DG}`}
-                    target="_blank"
-                  >
-                    Buy $DG
-                  </Button>
-                </span>
-              </span>
-            </Popup>
-
-            {copied ? (
-              <div className={copied ? 'copied-toast' : 'copied-toast hidden'}>
-                <h3 className="copied-text">Wallet address copied!</h3>
-              </div>
-            ) : null}
-          </div>
+          <ModalPopup />
         </span>
       );
     } else {
