1a5ece9751e232330257c36ff795e9ff1d33f9c5
0xsaxit <41426816+0xsaxit@users.noreply.github.com>
2021-07-04 13:35:28 -0500

    fix mobile button

--- components/home/MenuTop.js
+++ components/home/MenuTop.js
@@ -246,7 +246,7 @@ const MenuTop = (props) => {
                       className={menuStyle[1]}
                       id="dropdown-menu-items"
                     >
-                      Token Ecosystem
+                      DG Ecosystem
                     </Menu.Item>
                   </Link>
 
@@ -310,7 +310,7 @@ const MenuTop = (props) => {
 
         <Link href="/dg">
           <Menu.Item className={menuStyle[2]} id={getLinkStyles('/dg')}>
-            Token Ecosystem
+            DG Ecosystem
           </Menu.Item>
         </Link>
 
