930938b70953f7827cb8643e132e535d3ba8f820
fazlerabi <fazle.rabbi.dev@gmail.com>
2021-07-14 14:24:59 -0400

    Footer cleanups

--- components/home/Footer/index.js
+++ components/home/Footer/index.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import cn from 'classnames';
-import { Accordion } from 'semantic-ui-react';
+import { Button } from 'semantic-ui-react';
 import { useMediaQuery } from 'hooks';
 import Link from 'next/link';
 import styles from './Footer.module.scss';
@@ -29,7 +29,12 @@ const Footer = () => {
     <>
       <div className={styles.footer_container}>
         <div className={cn(styles.grid_container, 'd-flex flex-column')}>
-          <div className="d-flex flex-md-row flex-column w-100 mb-6">
+          <div
+            className={cn(
+              'd-flex flex-md-row flex-column w-100',
+              mobile ? 'mb-0' : 'mb-6'
+            )}
+          >
             <span className={styles.grid_column}>
               <p className={styles.footer_header}> Token </p>
               <Link href="/dg">
@@ -95,7 +100,29 @@ const Footer = () => {
                 </p>
               </Link>
 
-              <div className={styles.input_placeholder}>Enter your email</div>
+              <div className={styles.input_placeholder}>
+                {' '}
+                <Button
+                  color="blue"
+                  className={styles.sign_up_newsletter}
+                  href="https://decentralgames.substack.com/embed"
+                  target="_blank"
+                >
+                  Sign Up For Substack
+                  <svg
+                    width="14"
+                    height="14"
+                    viewBox="0 0 18 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M17.6152 13.2617V2.50879C17.6152 1.50977 16.9385 0.822266 15.9287 0.822266H5.16504C4.21973 0.822266 3.51074 1.54199 3.51074 2.40137C3.51074 3.26074 4.24121 3.92676 5.1543 3.92676H9.21484L12.5342 3.80859L10.6758 5.46289L1.28711 14.8623C0.932617 15.2168 0.739258 15.6357 0.739258 16.0654C0.739258 16.9033 1.52344 17.6982 2.37207 17.6982C2.80176 17.6982 3.20996 17.5049 3.5752 17.1504L12.9746 7.76172L14.6396 5.90332L14.5 9.10449V13.2725C14.5 14.1963 15.166 14.916 16.0254 14.916C16.8955 14.916 17.6152 14.1855 17.6152 13.2617Z"
+                      fill="white"
+                    ></path>
+                  </svg>
+                </Button>
+              </div>
             </div>
           </div>
           {!onlyTablet && !mobile && (
@@ -123,7 +150,29 @@ const Footer = () => {
             </p>
           </Link>
 
-          <div className={styles.input_placeholder}>Enter your email</div>
+          <div className={styles.input_placeholder}>
+            {' '}
+            <Button
+              color="blue"
+              className={styles.sign_up_newsletter}
+              href="https://decentralgames.substack.com/embed"
+              target="_blank"
+            >
+              Sign Up For Substack
+              <svg
+                width="14"
+                height="14"
+                viewBox="0 0 18 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M17.6152 13.2617V2.50879C17.6152 1.50977 16.9385 0.822266 15.9287 0.822266H5.16504C4.21973 0.822266 3.51074 1.54199 3.51074 2.40137C3.51074 3.26074 4.24121 3.92676 5.1543 3.92676H9.21484L12.5342 3.80859L10.6758 5.46289L1.28711 14.8623C0.932617 15.2168 0.739258 15.6357 0.739258 16.0654C0.739258 16.9033 1.52344 17.6982 2.37207 17.6982C2.80176 17.6982 3.20996 17.5049 3.5752 17.1504L12.9746 7.76172L14.6396 5.90332L14.5 9.10449V13.2725C14.5 14.1963 15.166 14.916 16.0254 14.916C16.8955 14.916 17.6152 14.1855 17.6152 13.2617Z"
+                  fill="white"
+                ></path>
+              </svg>
+            </Button>
+          </div>
         </div>
 
         {mobile && (
