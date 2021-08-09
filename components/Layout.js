bf60482dae6779c4d7e82787b76f5b5aa21a268e
0xsaxit <41426816+0xsaxit@users.noreply.github.com>
2021-08-09 16:59:21 +0100

    Revert "Menu fix on undefined window"
    This reverts commit c2886685965f0bf3b51c14d5642502940738738d.

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,8 @@ import { GlobalStyles } from '../static/css/global';
 import { useRouter } from 'next/router';
 import Footer from './home/Footer';
 
-const Layout = props => {
+
+const Layout = (props) => {
   // get theme (light or dark mode) from the Context API store
   const [state, dispatch] = useContext(GlobalContext);
   const router = useRouter();
@@ -37,16 +38,16 @@ const Layout = props => {
     <ThemeProvider theme={themeMode}>
       <GlobalStyles />
 
-      {typeof window === 'undefined' ||
-      typeof window.matchMedia === 'undefined' ? (
-        <></>
+      {router.pathname === '/' ? (
+        <MenuTop isHomePage={true} />
       ) : (
-        <MenuTop />
+        <MenuTop isHomePage={false} />
       )}
 
       {props.children}
 
       <Footer />
+
     </ThemeProvider>
   );
 };
