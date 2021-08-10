4331c33711107ea9cd9bdfd9b855c3d8f558c7a2
fazlerabi <fazle.rabbi.dev@gmail.com>
2021-08-10 10:31:25 -0400

    Fix poker history

--- components/content/ContentAccount/History/index.js
+++ components/content/ContentAccount/History/index.js
@@ -1,32 +1,55 @@
 import { useEffect, useContext, useState } from 'react';
 import { GlobalContext } from 'store/index';
 import cn from 'classnames';
-import { get } from 'lodash';
+import { get, map, sumBy } from 'lodash';
 import Global from 'components/Constants';
 import Images from 'common/Images';
-import { Icon, Button, Grid, Table } from 'semantic-ui-react';
+import poker from 'common/Poker';
+import { Modal, Button, Grid, Table } from 'semantic-ui-react';
 import Aux from 'components/_Aux';
 import styles from './History.module.scss';
-import ModalBreakdown from 'components/modal/ModalBreakdown';
 
 function History({ state }) {
   // get user's transaction history from the Context API store
-  const dataHistory = state.transactions[0];
-  const dataPlay = state.transactions[1];
-  const dataPoker = state.transactions[2];
-  console.log('?????');
-  console.log(dataPoker);
-
-  // console.log('state: ', state);
+  const [dataHistory, dataPlay, dataPoker] = state.transactions;
 
   // define local variables
-  const [dataPage, setDataPage] = useState([]);
-  const [dataPageTwo, setDataPageTwo] = useState([]);
-  const [dataPageThree, setDataPageThree] = useState([]);
+  const [open, setOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
-  const [utm, setUtm] = useState('');
 
-  const maximumCount = 100; // ***** we should limit the data being returned from the server to 100 rows *****
+  const transactions = dataHistory.filter(
+    h =>
+      get(h, 'type', '').includes('Deposit') ||
+      get(h, 'type', '').includes('Withdrawal')
+  );
+
+  const newPokerData = dataPoker.map(poker => {
+    const userInfoPlayIDs = map(
+      poker.tableData,
+      'playerHandData.1.userPlayInfoID'
+    );
+
+    return {
+      ...poker,
+      betAmount: sumBy(dataPlay, o => {
+        if (userInfoPlayIDs.includes(o._id)) return o.betAmount;
+        return 0;
+      }),
+      amountWin: sumBy(dataPlay, o => {
+        if (userInfoPlayIDs.includes(o._id)) return o.amountWin;
+        return 0;
+      }),
+      gameType: 9,
+      coinName: 'PLAY',
+    };
+  });
+
+  const playData = [
+    ...dataPlay.filter(
+      p => get(p, 'gameType', 0) < 10 && get(p, 'gameType', 0) !== 9
+    ),
+    ...newPokerData,
+  ];
 
   /////////////////////////////////////////////////////////////////////////////////////////
   /////////////////////////////////////////////////////////////////////////////////////////
@@ -36,56 +59,17 @@ function History({ state }) {
     }
   }, [state.transactions]);
 
-  useEffect(() => {
-    if (!isLoading) {
-      let result = {};
-      let resultTwo = {};
-      let i;
-      let j;
-      let history = [];
-      let gameplay = [];
-
-      result = dataHistory.slice(0, maximumCount);
-      resultTwo = dataPlay.slice(0, maximumCount);
-
-      // console.log(resultTwo);
-
-      for (i = 0; i < result.length; i++) {
-        const resultType = get(result, `${i}.type`, '');
-        if (
-          resultType.includes('Deposit') ||
-          resultType.includes('Withdrawal')
-        ) {
-          history.push(result[i]);
-        }
-      }
-
-      for (j = 0; j < resultTwo.length; j++) {
-        const resultTypeTwo = get(resultTwo, `${j}.gameType`, '');
-        console.log(resultTypeTwo);
-        if (
-          resultTypeTwo < 10
-        ) {
-          gameplay.push(resultTwo[j]);
-        }
-      }
-
-      setDataPage(history);
-      setDataPageTwo(gameplay);
-    }
-  }, [isLoading]);
-
   return (
     <Aux>
       <div className={styles.history_container}>
         <h1 className={styles.title}>Recent transactions</h1>
-        {!dataPage.length ? (
+        {!transactions.length ? (
           <div className={styles.error_container}>
             <p className={styles.error_state}>No Recent Transactions</p>
           </div>
         ) : (
           <Grid>
-            {dataPage.map((row, i) => {
+            {transactions.map((row, i) => {
               const date = new Date(row.createdAt);
               const timestamp = date.toDateString();
               const amount = (row.amount / 100000000000000000).toFixed(2);
@@ -227,7 +211,7 @@ function History({ state }) {
       <div className={styles.history_container}>
         <h1 className={styles.title}>Gameplay History</h1>
         <div className="tx-box-overflow">
-          {dataPageTwo === 'false' ? null : (
+          {playData === 'false' ? null : (
             <Table fixed unstackable>
               <Table.Header>
                 <Table.Row>
@@ -253,12 +237,12 @@ function History({ state }) {
             </Table>
           )}
 
-          {dataPageTwo === 'false' ? (
+          {playData === 'false' ? (
             <div className={styles.error_container}>
               <p className={styles.error_state}>No Recent Gameplay History</p>
             </div>
           ) : (
-            dataPageTwo.map((row, i) => {
+            playData.map((row, i) => {
               const date = new Date(row.createdAt);
               const timestamp = date.toLocaleString();
               const amount = Number(row.betAmount) / Global.CONSTANTS.FACTOR;
@@ -273,6 +257,8 @@ function History({ state }) {
                 action = 'Backgammon';
               } else if (row.gameType === 7 || row.gameType === 4) {
                 action = 'Blackjack';
+              } else if (row.gameType === 9) {
+                action = 'Poker';
               }
 
               let style = '';
@@ -463,7 +449,134 @@ function History({ state }) {
                                 </svg>
                               </Button>
                             </Aux>
-                          ) : null}
+                          ) : (
+                            <Modal
+                              className={styles.menu_info_modal}
+                              onClose={() => setOpen(false)}
+                              onOpen={() => setOpen(true)}
+                              open={open}
+                              close
+                              size="tiny"
+                              centered={true}
+                              trigger={
+                                <Button className={styles.session_history}>
+                                  Session History
+                                </Button>
+                              }
+                            >
+                              <div>
+                                <span
+                                  className={styles.button_close}
+                                  onClick={() => setOpen(false)}
+                                >
+                                  <svg
+                                    width="12"
+                                    height="12"
+                                    viewBox="0 0 12 12"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                  >
+                                    <path
+                                      d="M0.464355 9.65869C0.0952148 10.0344 0.0754395 10.7266 0.477539 11.1221C0.879639 11.5242 1.56519 11.511 1.94092 11.1353L5.65869 7.41748L9.36987 11.1287C9.75879 11.5242 10.4312 11.5176 10.8267 11.1155C11.2288 10.72 11.2288 10.0476 10.8398 9.65869L7.12866 5.94751L10.8398 2.22974C11.2288 1.84082 11.2288 1.16846 10.8267 0.772949C10.4312 0.37085 9.75879 0.37085 9.36987 0.759766L5.65869 4.47095L1.94092 0.753174C1.56519 0.384033 0.873047 0.364258 0.477539 0.766357C0.0820312 1.16846 0.0952148 1.854 0.464355 2.22974L4.18213 5.94751L0.464355 9.65869Z"
+                                      fill="white"
+                                    />
+                                  </svg>
+                                </span>
+                              </div>
+                              <div>
+                                <h1 className={styles.title}>
+                                  Poker Session History
+                                </h1>
+                                <p className={styles.date}>
+                                  {new Date(row.createdAt).toDateString()}
+                                </p>
+                                <div
+                                  className={cn('d-flex', styles.main_wrapper)}
+                                >
+                                  <div className={styles.your_hand}>
+                                    <p className={styles.subtitle}>Your Hand</p>
+                                    {row.tableData.map(item => {
+                                      return (
+                                        <div className={styles.hand_row}>
+                                          {get(
+                                            item,
+                                            'playerHandData.1.hand',
+                                            []
+                                          ).map(card => {
+                                            return (
+                                              <img
+                                                src={
+                                                  poker[card[1] - 1][card[0]]
+                                                }
+                                              />
+                                            );
+                                          })}
+                                        </div>
+                                      );
+                                    })}
+                                  </div>
+                                  <div className={styles.table_cards}>
+                                    <p className={styles.subtitle}>
+                                      Table Cards
+                                    </p>
+                                    {row.tableData.map(item => {
+                                      return (
+                                        <div>
+                                          {item.communityCards.map(card => {
+                                            return (
+                                              <img
+                                                src={
+                                                  poker[card[1] - 1][card[0]]
+                                                }
+                                              />
+                                            );
+                                          })}
+                                        </div>
+                                      );
+                                    })}
+                                  </div>
+                                  <div className={styles.pay_out}>
+                                    <p className={styles.subtitle}>Payout</p>
+                                    {row.tableData.map(item => {
+                                      const userPlayInfoID = get(
+                                        item,
+                                        'playerHandData.1.userPlayInfoID',
+                                        ''
+                                      );
+
+                                      const amountWin =
+                                        get(
+                                          dataPlay.filter(
+                                            play => play._id === userPlayInfoID
+                                          ),
+                                          '0.amountWin',
+                                          0
+                                        ) / Global.CONSTANTS.FACTOR;
+
+                                      return (
+                                        <p className={styles.pay_out_call}>
+                                          {row.coinName === 'DAI' ? (
+                                            <img src={Images.DAI_CIRCLE} />
+                                          ) : row.coinName === 'MANA' ? (
+                                            <img src={Images.MANA_CIRCLE} />
+                                          ) : row.coinName === 'USDT' ? (
+                                            <img src={Images.USDT_CIRCLE} />
+                                          ) : row.coinName === 'ATRI' ? (
+                                            <img src={Images.ATRI_CIRCLE} />
+                                          ) : row.coinName === 'WETH' ? (
+                                            <img src={Images.ETH_CIRCLE} />
+                                          ) : (
+                                            <img src={Images.PLAY_CIRCLE} />
+                                          )}
+                                          {amountWin}&nbsp;{row.coinName}
+                                        </p>
+                                      );
+                                    })}
+                                  </div>
+                                </div>
+                              </div>
+                            </Modal>
+                          )}
 
                           {row.coinName !== 'PLAY' ? (
                             <Aux>
