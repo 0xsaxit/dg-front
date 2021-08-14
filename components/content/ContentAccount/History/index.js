5ec17780c65a8a6d4cb6ed4515a76abf09fdce65
fazlerabi <fazle.rabbi.dev@gmail.com>
2021-08-14 09:40:05 -0400

    Fix game play

--- components/content/ContentAccount/History/index.js
+++ components/content/ContentAccount/History/index.js
@@ -29,16 +29,25 @@ function History({ state }) {
       'playerHandData.1.userPlayInfoID'
     );
 
+    let betAmount = 0;
+    for (let i = 0; i < dataPlay.length; i++) {
+      if (userInfoPlayIDs.includes(dataPlay[i]._id)) {
+        betAmount = dataPlay[i].betAmount;
+        break;
+      }
+    }
+
+    let amountWin = 0;
+    for (let i = 0; i < dataPlay.length; i++) {
+      if (userInfoPlayIDs.includes(dataPlay[i]._id)) {
+        amountWin = dataPlay[i].amountWin;
+      }
+    }
+
     return {
       ...poker,
-      betAmount: sumBy(dataPlay, o => {
-        if (userInfoPlayIDs.includes(o._id)) return o.betAmount;
-        return 0;
-      }),
-      amountWin: sumBy(dataPlay, o => {
-        if (userInfoPlayIDs.includes(o._id)) return o.amountWin;
-        return 0;
-      }),
+      betAmount,
+      amountWin,
       gameType: 9,
       coinName: 'PLAY',
     };
@@ -345,7 +354,7 @@ function History({ state }) {
                             }}
                           />
                         )}
-                        -{amount} {row.coinName}
+                        {amount > 0 ? `+${amount}` : amount} {row.coinName}
                       </Table.Cell>
                       <Table.Cell style={{ width: '180px' }}>
                         {row.coinName === 'DAI' ? (
@@ -415,7 +424,7 @@ function History({ state }) {
                             }}
                           />
                         )}
-                        +{result} {row.coinName}
+                        {result > 0 ? `+${result}` : result} {row.coinName}
                       </Table.Cell>
                       <Table.Cell style={{ width: '240px' }}>
                         {timestamp}
