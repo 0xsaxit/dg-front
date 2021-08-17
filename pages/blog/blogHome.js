829f088a50a5e14499de25df6c963a5c7bb5b22f
James_Sayer <james.sayer0908@gmail.com>
2021-08-17 07:51:02 -0500

    Bugfix for News & Blogs page

--- pages/blog/blogHome.js
+++ pages/blog/blogHome.js
@@ -7,6 +7,8 @@ import Aux from 'components/_Aux';
 import Images from 'common/Images';
 import ButterCMS from 'common/ButterCMS';
 import cn from 'classnames';
+import Metamask from 'assets/svg/metamask.svg';
+import GreaterThan from 'assets/svg/greaterthan.svg';
 
 import styles from './blog.module.scss';
 
@@ -49,9 +51,7 @@ const blog = () => {
         >
           <span className="d-flex align-items-center justify-content-center">
             Sign Up For Substack
-            <svg style={{ marginLeft: '12px' }} width="14" height="14" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M17.6152 13.2617V2.50879C17.6152 1.50977 16.9385 0.822266 15.9287 0.822266H5.16504C4.21973 0.822266 3.51074 1.54199 3.51074 2.40137C3.51074 3.26074 4.24121 3.92676 5.1543 3.92676H9.21484L12.5342 3.80859L10.6758 5.46289L1.28711 14.8623C0.932617 15.2168 0.739258 15.6357 0.739258 16.0654C0.739258 16.9033 1.52344 17.6982 2.37207 17.6982C2.80176 17.6982 3.20996 17.5049 3.5752 17.1504L12.9746 7.76172L14.6396 5.90332L14.5 9.10449V13.2725C14.5 14.1963 15.166 14.916 16.0254 14.916C16.8955 14.916 17.6152 14.1855 17.6152 13.2617Z" fill="white"/>
-            </svg>
+            <Metamask />
           </span>
         </Button>
         <p className={styles.substack_text}> Or read on{' '}
@@ -62,7 +62,7 @@ const blog = () => {
       </div>
 
       <div className={styles.account_other_tabs} style={{ marginTop: '-50px' }}>
-        <div className="ml-0">
+        <div className="ml-0 mb-4">
           <span className={styles.account_featured_post}>
             Featured Post
           </span>
@@ -115,7 +115,7 @@ const blog = () => {
               <div className={cn("mt-0", styles.account_other_tabs)}>
                 <div className="ml-0">
                   <span className={cn("d-flex", styles.account_other_p)}>
-                    <span className="d-flex justify-content-between w-100">
+                    <span className="mb-4 d-flex justify-content-between w-100">
                       <span 
                         style={{ 
                           fontSize: '24px',
@@ -135,19 +135,7 @@ const blog = () => {
                           <Button className={styles.all_button}>
                             <span>
                               See All
-                              <svg
-                                style={{ marginLeft: '4px' }}
-                                width="6"
-                                height="9"
-                                viewBox="0 0 6 9"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                              >
-                                <path
-                                  d="M5.56543 4.82715C5.56104 4.59424 5.48193 4.40967 5.29297 4.2251L1.98389 0.990723C1.84326 0.854492 1.68066 0.78418 1.47852 0.78418C1.07422 0.78418 0.740234 1.11377 0.740234 1.51367C0.740234 1.71582 0.82373 1.90039 0.977539 2.0542L3.84277 4.82275L0.977539 7.6001C0.82373 7.74951 0.740234 7.93408 0.740234 8.14062C0.740234 8.54053 1.07422 8.87012 1.47852 8.87012C1.67627 8.87012 1.84326 8.8042 1.98389 8.66357L5.29297 5.4292C5.48193 5.24463 5.56543 5.05566 5.56543 4.82715Z"
-                                  fill="white"
-                                />
-                              </svg>
+                              <GreaterThan />
                             </span>
                           </Button>
                         </Link>
