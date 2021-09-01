69230da7850c039c5aa64c66caa7c7ff8c757abe
0xsaxit <41426816+0xsaxit@users.noreply.github.com>
2021-09-01 12:32:15 +0100

    new ice tout, small home ui fixes

--- components/home/MenuTop/index.js
+++ components/home/MenuTop/index.js
@@ -12,13 +12,14 @@ import ButtonConnect from '../../button/ButtonConnect/index.js';
 import styles from './MenuTop.module.scss';
 import MessageToast from 'components/home/MessageToast';
 
+
 const MenuTop = props => {
+
   // get token balances from the Context API store
   const [state, dispatch] = useContext(GlobalContext);
   const isTablet = useMediaQuery('(min-width: 1100px)');
   const isMobile = useMediaQuery('(min-width: 768px)');
-  // define local variables
-  // const [isDarkMode, setDarkMode] = useState(false);
+  const isSquished = useMediaQuery('(min-width: 820px)');
   const [open, setOpen] = useState(false);
   const [utm, setUtm] = useState('');
   const [scrollState, setScrollState] = useState('top');
@@ -305,7 +306,10 @@ const MenuTop = props => {
             state.userStatus >= 4 ? '' : 'd-none'
           )}
         >
-          <ModalInfo />
+          {isSquished ?
+            <ModalInfo /> :
+            null
+          }
           <ModalPopup />
         </span>
         <span
