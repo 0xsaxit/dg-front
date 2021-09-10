104e00cbee8a119091cfd17ab15e71896f853cb5
Steve Becerra <33670914+mobiman1@users.noreply.github.com>
2021-09-10 22:40:50 +0700

    enable WETH and mint API call

--- components/modal/ModalEthAuth/index.js
+++ components/modal/ModalEthAuth/index.js
@@ -1,13 +1,201 @@
-import React, { useState } from 'react'
-import cn from 'classnames';
+import { useState, useEffect, useContext } from 'react';
+import { GlobalContext } from '../../../store';
+import Biconomy from '@biconomy/mexa';
+import Web3 from 'web3';
 import { Modal, Button } from 'semantic-ui-react';
 import styles from './ModalEthAuth.module.scss';
+import ABI_CHILD_TOKEN_WETH from '../../ABI/ABIChildTokenWETH';
+import MetaTx from '../../../common/MetaTx';
+import Fetch from '../../../common/Fetch';
+import Aux from '../../_Aux';
+import Global from '../../Constants';
 
+const ModalEthAuth = props => {
+  // dispatch user's treasury contract active status to the Context API store
+  const [state, dispatch] = useContext(GlobalContext);
 
-const ModalEthAuth = (props) => {
-
+  // define local variables
+  const [tokenContract, setTokenContract] = useState({});
+  const [web3, setWeb3] = useState({});
+  const [spenderAddress, setSpenderAddress] = useState('');
+  const [authStatus, setAuthStatus] = useState(false);
   const [open, setOpen] = useState(false);
+  const [minting, setMinting] = useState(false);
+  const [buttonMessage, setButtonMessage] = useState('Proceed to Mint');
 
+  let authorizationStatus = 'false';
+
+  /////////////////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////////////////
+  // initialize Web3 providers and create token contract instance
+  useEffect(() => {
+    if (state.userStatus >= 4) {
+      const web3 = new Web3(window.ethereum); // pass MetaMask provider to Web3 constructor
+      setWeb3(web3);
+
+      const biconomy = new Biconomy(
+        new Web3.providers.HttpProvider(Global.CONSTANTS.MATIC_URL),
+        {
+          apiKey: Global.KEYS.BICONOMY_API_1,
+          debug: true,
+        }
+      );
+      const getWeb3 = new Web3(biconomy); // pass Biconomy object to Web3 constructor
+
+      const spenderAddress = Global.ADDRESSES.ICE_REGISTRANT_ADDRESS;
+      setSpenderAddress(spenderAddress);
+
+      const tokenContract = new getWeb3.eth.Contract(
+        ABI_CHILD_TOKEN_WETH,
+        Global.ADDRESSES.CHILD_TOKEN_ADDRESS_WETH
+      );
+
+      setTokenContract(tokenContract);
+
+      biconomy
+        .onEvent(biconomy.READY, () => {
+          console.log('Mexa is Ready: Approve ETH (wearables)');
+        })
+        .onEvent(biconomy.ERROR, (error, message) => {
+          console.error(error);
+        });
+    }
+  }, [state.userStatus]);
+
+  useEffect(() => {
+    if (state.tokenAuthorizations.WETH_AUTHORIZATION === true) {
+      authorizationStatus = 'True';
+    } else {
+      authorizationStatus = 'False';
+    }
+
+    setAuthStatus(authorizationStatus);
+  }, [state.tokenAuthorizations]);
+
+  /////////////////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////////////////
+  // helper functions
+  function approveWETH() {
+    return (
+      <Aux>
+        <p className={styles.header}>Mint ICE Wearable</p>
+
+        <p className={styles.description}>
+          To mint a new ICE wearable, you first need to authorize Ethereum
+          transactions for the amount of{' '}
+          {Global.CONSTANTS.WEARABLE_AMOUNT / Global.CONSTANTS.FACTOR} ETH
+        </p>
+
+        <p className={styles.description}>
+          ETH authorization status: {authStatus}
+        </p>
+
+        <div className={styles.upgrade_inner_container}>
+          <div className={styles.upgrade_area}>
+            <svg
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle
+                cx="16"
+                cy="16"
+                r="14"
+                stroke="white"
+                stroke-opacity="0.25"
+                stroke-width="4"
+              />
+            </svg>
+
+            {authStatus ? (
+              <Button className={styles.enabled_button} disabled>
+                Enable ETH
+              </Button>
+            ) : (
+              <Button
+                className={styles.enabled_button}
+                onClick={() => metaTransaction()}
+              >
+                Enable ETH
+              </Button>
+            )}
+
+            <div className={styles.upgrade_right}>
+              <p className={styles.upgrade_top_text}>Authorize ETH</p>
+              <p className={styles.upgrade_bottom_text}>
+                Enables ETH Transaction
+              </p>
+            </div>
+          </div>
+        </div>
+      </Aux>
+    );
+  }
+
+  async function mintToken() {
+    setMinting(true);
+    setButtonMessage('Minting Token...');
+
+    const json = await Fetch.MINT_TOKEN();
+
+    // console.log('return data...');
+    // console.log(json);
+
+    if (json.status) {
+      // update global state token authorizations
+      const refresh = !state.refreshTokenAuth;
+
+      dispatch({
+        type: 'refresh_token_auth',
+        data: refresh,
+      });
+
+      setButtonMessage('Proceed to Mint');
+    } else {
+      setAuthStatus(false);
+      setButtonMessage('Token Minting Error');
+    }
+
+    setMinting(false);
+  }
+
+  // Biconomy API meta-transaction. User must authorize WETH token contract to access their funds
+  async function metaTransaction() {
+    try {
+      console.log('authorize amount: ' + Global.CONSTANTS.WEARABLE_AMOUNT);
+
+      // get function signature and send Biconomy API meta-transaction
+      let functionSignature = tokenContract.methods
+        .approve(spenderAddress, Global.CONSTANTS.WEARABLE_AMOUNT)
+        .encodeABI();
+
+      const txHash = await MetaTx.executeMetaTransaction(
+        6,
+        functionSignature,
+        tokenContract,
+        state.userAddress,
+        web3
+      );
+
+      if (txHash === false) {
+        console.log('Biconomy meta-transaction failed');
+      } else {
+        console.log('Biconomy meta-transaction hash: ' + txHash);
+
+        // update global state token authorizations
+        const refresh = !state.refreshTokenAuth;
+
+        dispatch({
+          type: 'refresh_token_auth',
+          data: refresh,
+        });
+      }
+    } catch (error) {
+      console.log('WETH authorization error: ' + error);
+    }
+  }
 
   return (
     <>
@@ -17,11 +205,7 @@ const ModalEthAuth = (props) => {
         onOpen={() => setOpen(true)}
         open={open}
         close
-        trigger={
-          <Button className={styles.open_button}>
-            Mint
-          </Button>
-        }
+        trigger={<Button className={styles.open_button}>Mint</Button>}
       >
         <div className={styles.close_icon} onClick={() => setOpen(false)}>
           <span className={styles.button_close}>
@@ -40,30 +224,35 @@ const ModalEthAuth = (props) => {
           </span>
         </div>
 
-        <div style={{ color: 'white', display: 'flex', justifyContent: 'center', textAlign: 'center' }}>
+        <div
+          style={{
+            color: 'white',
+            display: 'flex',
+            justifyContent: 'center',
+            textAlign: 'center',
+          }}
+        >
           <div className={styles.upgrade_container}>
-            <p className={styles.header}> Mint ICE Wearable </p>
+            {approveWETH()}
 
-            <p className={styles.description}>
-              To mint a new ICE wearable, you first need to authorize Ethereum transactions.
-            </p>
-
-            <div className={styles.upgrade_inner_container}>
-              <div className={styles.upgrade_area}>
-                <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <circle cx="16" cy="16" r="14" stroke="white" stroke-opacity="0.25" stroke-width="4"/>
-                </svg>
-
-                <div className={styles.upgrade_right}>
-                  <p className={styles.upgrade_top_text}> Authorize ETH </p>
-                  <p className={styles.upgrade_bottom_text}> Enables ETH Transaction </p>
-                </div>
-              </div>
-            </div>
-            
-            <Button className={styles.proceed_button}>
-              Proceed to Mint
-            </Button>
+            {!minting ? (
+              authStatus ? (
+                <Button
+                  className={styles.proceed_button}
+                  onClick={() => mintToken()}
+                >
+                  {buttonMessage}
+                </Button>
+              ) : (
+                <Button disabled className={styles.proceed_button}>
+                  {buttonMessage}
+                </Button>
+              )
+            ) : (
+              <Button disabled className={styles.proceed_button}>
+                {buttonMessage}
+              </Button>
+            )}
           </div>
         </div>
       </Modal>
