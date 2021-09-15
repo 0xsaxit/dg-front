799cf65a767fb3f2440e1dd98aca52fcd290ba30
Steve Becerra <33670914+mobiman1@users.noreply.github.com>
2021-09-15 12:30:38 +0700

    authorized ETH state fix

--- components/modal/ModalEthAuth/index_original.js
+++ components/modal/ModalEthAuth/index_original.js
@@ -31,6 +31,7 @@ const ModalEthAuth = props => {
   const [minting, setMinting] = useState(false);
   const [buttonMessage, setButtonMessage] = useState('Proceed to Mint');
   const [clicked, setClicked] = useState(false);
+  const [done, setDone] = useState(false);
 
   /////////////////////////////////////////////////////////////////////////////////////////
   /////////////////////////////////////////////////////////////////////////////////////////
@@ -107,44 +108,6 @@ const ModalEthAuth = props => {
   /////////////////////////////////////////////////////////////////////////////////////////
   /////////////////////////////////////////////////////////////////////////////////////////
   // helper functions
-  function checkButton(buttonState) {
-    return (
-      <svg
-        width="32"
-        height="32"
-        viewBox="0 0 32 32"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        {buttonState === 'authed' ? (
-          <Aux>
-            <circle cx="16" cy="16" r="16" fill="#35AB3A" />
-            <path
-              d="M14.7197 23.5601C15.4375 23.5601 15.9941 23.3037 16.375 22.7471L23.084 12.8594C23.3477 12.4712 23.4648 12.0684 23.4648 11.7095C23.4648 10.6841 22.6445 9.90771 21.5898 9.90771C20.8794 9.90771 20.4106 10.1641 19.9785 10.8452L14.6904 19.0483L12.105 16.1553C11.7388 15.7378 11.2993 15.54 10.7134 15.54C9.65137 15.54 8.86768 16.3164 8.86768 17.3491C8.86768 17.8252 8.99219 18.1914 9.39502 18.6455L13.1523 22.8862C13.5698 23.355 14.0825 23.5601 14.7197 23.5601Z"
-              fill="white"
-            />
-          </Aux>
-        ) : 'empty' ? (
-          <circle
-            cx="16"
-            cy="16"
-            r="14"
-            stroke="white"
-            stroke-opacity="0.25"
-            stroke-width="4"
-          />
-        ) : 'clicked' ? (
-          <Aux>
-            <circle cx="16" cy="16" r="16" fill="#35AB3A" />
-            <circle cx="16" cy="15.7" r="2.7" fill="white" />
-            <circle cx="8.7" cy="15.7" r="2.7" fill="white" />
-            <circle cx="23.3001" cy="15.7" r="2.7" fill="white" />
-          </Aux>
-        ) : null}
-      </svg>
-    );
-  }
-
   function approveWETH() {
     return (
       <Aux>
@@ -159,13 +122,51 @@ const ModalEthAuth = props => {
         <div className={styles.upgrade_inner_container}>
           <div className={styles.upgrade_area}>
             <span onClick={() => metaTransaction()}>
-              {authStatus
-                ? checkButton('authed')
-                : !clicked
-                ? checkButton('empty')
-                : clicked
-                ? checkButton('clicked')
-                : null}
+              {!clicked && !done ? (
+                <svg
+                  width="32"
+                  height="32"
+                  viewBox="0 0 32 32"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <circle
+                    cx="16"
+                    cy="16"
+                    r="14"
+                    stroke="white"
+                    stroke-opacity="0.25"
+                    stroke-width="4"
+                  />
+                </svg>
+              ) : clicked && !done ? (
+                <svg
+                  width="32"
+                  height="32"
+                  viewBox="0 0 32 32"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <circle cx="16" cy="16" r="16" fill="#35AB3A" />
+                  <circle cx="16" cy="15.7" r="2.7" fill="white" />
+                  <circle cx="8.7" cy="15.7" r="2.7" fill="white" />
+                  <circle cx="23.3001" cy="15.7" r="2.7" fill="white" />
+                </svg>
+              ) : (
+                <svg
+                  width="32"
+                  height="32"
+                  viewBox="0 0 32 32"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <circle cx="16" cy="16" r="16" fill="#35AB3A" />
+                  <path
+                    d="M14.7197 23.5601C15.4375 23.5601 15.9941 23.3037 16.375 22.7471L23.084 12.8594C23.3477 12.4712 23.4648 12.0684 23.4648 11.7095C23.4648 10.6841 22.6445 9.90771 21.5898 9.90771C20.8794 9.90771 20.4106 10.1641 19.9785 10.8452L14.6904 19.0483L12.105 16.1553C11.7388 15.7378 11.2993 15.54 10.7134 15.54C9.65137 15.54 8.86768 16.3164 8.86768 17.3491C8.86768 17.8252 8.99219 18.1914 9.39502 18.6455L13.1523 22.8862C13.5698 23.355 14.0825 23.5601 14.7197 23.5601Z"
+                    fill="white"
+                  />
+                </svg>
+              )}
             </span>
 
             <div className={styles.upgrade_right}>
@@ -238,6 +239,8 @@ const ModalEthAuth = props => {
           type: 'refresh_token_auth',
           data: refresh,
         });
+
+        setDone(true);
       }
     } catch (error) {
       console.log('WETH authorization error: ' + error);
