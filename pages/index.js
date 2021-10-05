2a774fc758fd54ec21ea7b627c646d38ee8ed5b6
Yamada <cool.developer24@gmail.com>
2021-10-04 22:44:28 -0400

    update permission for dev server

--- pages/index.js
+++ pages/index.js
@@ -8,33 +8,17 @@ import { useEffect, useContext, useState } from 'react';
 import { GlobalContext } from '../store';
 
 const Index = () => {
-  const [state, dispatch] = useContext(GlobalContext);
+  const [state, dispatch] = useContext(GlobalContext);  
   return (
     <>
-      {state.userVerified ? (
-        <Layout>
-          <Header
-            title={Global.CONSTANTS.TITLE + ' | Be The House'}
-            description={Global.CONSTANTS.DESCRIPTION}
-            image={Images.SOCIAL_SHARE}
-          />
-          <DGHome />
-        </Layout>
-      ) : (
-        <div
-          style={{
-            background: 'gray',
-            textAlign: 'center',
-            fontSize: '18px',
-            height: '35px',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          You don’t have permission to view this page!
-        </div>
-      )}
+      <Layout>
+        <Header
+          title={Global.CONSTANTS.TITLE + ' | Be The House'}
+          description={Global.CONSTANTS.DESCRIPTION}
+          image={Images.SOCIAL_SHARE}
+        />
+        <DGHome />
+      </Layout>
     </>
   );
 };
