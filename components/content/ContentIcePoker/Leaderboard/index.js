b6d383564fe72e1df72c4bbe1e94dbc3f470c0b2
0xsaxit <41426816+0xsaxit@users.noreply.github.com>
2021-10-28 10:40:35 -0700

    merge to production

--- components/content/ContentIcePoker/Leaderboard/index.js
+++ components/content/ContentIcePoker/Leaderboard/index.js
@@ -84,7 +84,7 @@ const Leaderboard = () => {
                             </abbr>
                         </div>
                         <div className={styles.winnings}>
-                            <abbr>{state.iceAmounts.ICE_AVAILABLE_AMOUNT.toLocaleString()}</abbr>
+                            <abbr>--</abbr>
                             <img src="https://res.cloudinary.com/dnzambf4m/image/upload/v1635212177/FREE_Coin_c08hyk.png" alt="ice" />
                         </div>
                     </div>
