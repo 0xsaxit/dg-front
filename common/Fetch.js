2d5bb974cd84e9acfb8f8d49c84049714d247424
0xsaxit <bcsaxit@gmail.com>
2021-11-02 14:09:12 -0700

    Update logging

--- common/Fetch.js
+++ common/Fetch.js
@@ -1,6 +1,5 @@
 import call from 'common/API';
 import getConfig from 'next/config'
-
 import { ApiUrlsByAppEnv } from './environments'
 
 // This imports NODE_ENV from next.config.js
@@ -10,8 +9,7 @@ const { APP_ENV } = publicRuntimeConfig;
 // APP_ENV must be set in the .env.{environment} files
 export const API_BASE_URL = ApiUrlsByAppEnv[APP_ENV] || 'https://api.decentral.games';
 
-console.log('process.env.APP_ENV (NODE_ENV): ', process.env.APP_ENV)
-console.log('APP_ENV: ', APP_ENV)
+console.log('APP_ENV (NODE_ENV): ', APP_ENV)
 console.log('API_BASE_URL: ', API_BASE_URL)
 
 const apiCall = {
