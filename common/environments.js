dc999ae73cbcdacffa98b5fe6e062154a434d382
0xsaxit <bcsaxit@gmail.com>
2021-11-03 10:25:47 -0700

    Update environments for localhost with flag

--- common/environments.js
+++ common/environments.js
@@ -1,5 +1,7 @@
+const useLocalhostApis = false;
+
 export const ApiUrlsByAppEnv = {
-    localhost: 'localhost:5000',
+    localhost: useLocalhostApis ? 'http://localhost:5000' : 'https://api.decentral.games',
     testing:'https://api.testing.decentral.games',
     development: 'https://api.dev.decentral.games',
     staging: 'https://api.staging.decentral.games',
