c5036fc23acd39d53b8aa017d03887544932eef7
alex <alex@decentral.games>
2021-11-30 11:07:36 -0800

    Updated Delegate Modal UI

--- components/modal/ModalDelegate/index.js
+++ components/modal/ModalDelegate/index.js
@@ -164,7 +164,6 @@ const ModalDelegate = props => {
             <ul>
               <li>Let another player Play-to-Earn with your item</li>
               <li>Earn 30% of all ICE profits from their gameplay</li>
-              <li>Withdraw your delegation at any time.</li>
             </ul>
           </div>
         </div>
@@ -383,7 +382,12 @@ const ModalDelegate = props => {
                     </Button>
                   )}
 
-                  <Button className={styles.button_close}>Learn More</Button>
+                  <Button
+                    className={styles.button_close}
+                    onClick={() => {
+                      window.open("https://ice.decentral.games/ice-nft-wearables", "_blank");
+                    }}
+                  >Learn More</Button>
                 </div>
               </div>
 
