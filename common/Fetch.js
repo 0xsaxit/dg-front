b69faaf02b278b8ee3410dc204b4bfb18b08ffe1
Paul Lee <paul@decentral.games>
2021-12-16 07:14:48 -0600

    Revert Fetch.js API url

--- common/Fetch.js
+++ common/Fetch.js
@@ -7,7 +7,7 @@ const { publicRuntimeConfig } = getConfig();
 const { APP_ENV } = publicRuntimeConfig;
 
 // APP_ENV must be set in the .env.{environment} files
-export const API_BASE_URL = 'https://api.testing.decentral.games';
+export const API_BASE_URL = ApiUrlsByAppEnv[APP_ENV] || 'https://api.decentral.games';
 
 console.log('APP_ENV (NODE_ENV): ', APP_ENV);
 console.log('API_BASE_URL: ', API_BASE_URL);
