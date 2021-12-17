21b600031097fa4b89dccecfda45ebc4fc9b5e41
0xsaxit <41426816+0xsaxit@users.noreply.github.com>
2021-12-16 20:17:25 -0800

    mint modal fix

--- components/modal/ModalMintWearable/index.js
+++ components/modal/ModalMintWearable/index.js
@@ -207,12 +207,12 @@ const ModalMint = props => {
   function buttons() {
     return (
       <div className={styles.button_area}>
-        {state.userBalances[2][3] <
-          state.tokenAmounts.WETH_COST_AMOUNT ||
-          sstate.stakingBalances.BALANCE_USER_GOVERNANCE_OLD >= 
+        {(state.userBalances[2][3] <
+          state.tokenAmounts.WETH_COST_AMOUNT) &&
+          (state.stakingBalances.BALANCE_USER_GOVERNANCE_OLD >= 
           Global.CONSTANTS.DG_STAKED_AMOUNT ||
           xDG >= 
-          Global.CONSTANTS.XDG_STAKED_AMOUNT ? (
+          Global.CONSTANTS.XDG_STAKED_AMOUNT) ? (
           <Button className={styles.button_upgrade} disabled={true}>
             Mint Wearable
           </Button>
