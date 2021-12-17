4e10121e8cf24c0b918bd746f5ca40edca5c1bc9
0xsaxit <41426816+0xsaxit@users.noreply.github.com>
2021-12-16 18:59:46 -0800

    fix old dg

--- components/modal/ModalMintWearable/index.js
+++ components/modal/ModalMintWearable/index.js
@@ -125,7 +125,7 @@ const ModalMint = props => {
               />
             </div>
 
-            {state.DGBalances.DG_STAKED_AMOUNT >= 
+            {state.stakingBalances.BALANCE_USER_GOVERNANCE_OLD >= 
               Global.CONSTANTS.DG_STAKED_AMOUNT ||
               xDG >= 
               Global.CONSTANTS.XDG_STAKED_AMOUNT ? (       
@@ -207,10 +207,10 @@ const ModalMint = props => {
   function buttons() {
     return (
       <div className={styles.button_area}>
-        {state.userBalances[2][3] < 
-          state.tokenAmounts.WETH_COST_AMOUNT &&
-          state.DGBalances.DG_STAKED_AMOUNT >= 
-          Global.CONSTANTS.DG_STAKED_AMOUNT &&
+        {state.userBalances[2][3] <
+          state.tokenAmounts.WETH_COST_AMOUNT ||
+          sstate.stakingBalances.BALANCE_USER_GOVERNANCE_OLD >= 
+          Global.CONSTANTS.DG_STAKED_AMOUNT ||
           xDG >= 
           Global.CONSTANTS.XDG_STAKED_AMOUNT ? (
             <Button className={styles.button_upgrade} disabled={true}>
