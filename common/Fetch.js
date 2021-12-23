137c5da43c31a3f6d996ac04609d071b09f0b3c7
garchbol <ge.archbold@gmail.com>
2021-12-23 15:15:40 -0800

    remove FETCH console log

--- common/Fetch.js
+++ common/Fetch.js
@@ -195,7 +195,6 @@ const Fetch = {
   },
 
   GET_WEARABLE_INVENTORY: (address) => {
-    console.log(`${API_BASE_URL}/ice/getWearableInventory?address=${address}`)
     return call(
       `${API_BASE_URL}/ice/getWearableInventory?address=${address}`,
       'GET',
