a77d67f2158d057661b8d141d7613bc743c35881
leo-2408 <93679109+leo-2408@users.noreply.github.com>
2022-01-11 14:49:27 -0800

    test pr

--- components/common/cards/ICEWearableCard/ICEWearableCard.js
+++ components/common/cards/ICEWearableCard/ICEWearableCard.js
@@ -16,6 +16,7 @@ import Aux from '../../../_Aux';
 
 const ICEWearableCard = props => {
   // get user's wallet address from the Context API store
+  // hi just testing to see if George can approve
   const [state, dispatch] = useContext(GlobalContext);
 
   // define local variables
