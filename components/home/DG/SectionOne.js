3354e0d41d95991390c6109db23c87c92ae1c414
alex <alex@decentral.games>
2022-01-12 08:06:40 -0800

    Updated our home page designs
    AB#2507

--- components/home/DG/SectionOne.js
+++ components/home/DG/SectionOne.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Button, Icon } from 'semantic-ui-react';
+import { Button, Grid } from 'semantic-ui-react';
 import cn from 'classnames';
 import { useMediaQuery } from 'hooks';
 import Aux from 'components/_Aux';
@@ -16,7 +16,7 @@ function SectionOne(props) {
       <video
         className={styles.video}
         id="my-video"
-        src="https://res.cloudinary.com/dnzambf4m/video/upload/q_auto:best/v1622134332/Full_Screen_Background_Animation_uo9h6b.webm"
+        src="https://res.cloudinary.com/dnzambf4m/video/upload/v1641930582/ICE_Landing_Page_rusy2d.webm"
         type="video/mp4"
         frameBorder="0"
         autoPlay={props.autoPlay}
@@ -25,13 +25,7 @@ function SectionOne(props) {
       ></video>
 
       <div className={cn(styles.home_dashboard_content, 'container-fluid')}>
-        <div
-          className={cn(
-            'row',
-            styles.row,
-            mobile ? 'text-center' : 'text-left'
-          )}
-        >
+        <div className={cn('row', styles.row, mobile ? 'text-center' : 'text-left')} >
           <div className="col-md-12 col-lg-9 d-flex flex-column">
             <h1>
               {' '}
@@ -74,6 +68,26 @@ function SectionOne(props) {
           </div>
         </div>
 
+        <Grid className={styles.bottomRow}>
+          <Grid.Row>
+            <Grid.Column className={styles.section} computer={4} tablet={4} mobile={8}>
+              <h1>$59.2M</h1>
+              <p className={styles.color1}>In DG Treasury</p>
+            </Grid.Column>
+            <Grid.Column className={styles.section} computer={4} tablet={4} mobile={8}>
+              <h1>$3.2M</h1>
+              <p className={styles.color2}>Revenue This Month</p>
+            </Grid.Column>
+            <Grid.Column className={styles.section} computer={4} tablet={4} mobile={8}>
+              <h1>101K <img src="https://res.cloudinary.com/dnzambf4m/image/upload/v1641876355/user_z93mel.png" alt="user" /></h1>
+              <p className={styles.color3}>Monthly Metaverse Visitors</p>
+            </Grid.Column>
+            <Grid.Column className={styles.section} computer={4} tablet={4} mobile={8}>
+              <h1>108K <img src="https://res.cloudinary.com/dnzambf4m/image/upload/v1641876355/wallet_gqjtbf.png" alt="wallet" /></h1>
+              <p className={styles.color4}>Wallets Holding DG</p>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
         {/*<p className={styles.chevron_down}>
           <Icon name="chevron down" />
         </p>*/}
