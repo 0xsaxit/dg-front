63458c63adf1cea2a6a38a5334bd0e4162598986
garchbol <ge.archbold@gmail.com>
2022-01-26 15:30:45 -0800

    remove extra check

--- components/modal/ModalInfo/index.js
+++ components/modal/ModalInfo/index.js
@@ -129,9 +129,7 @@ const ModalInfo = () => {
       setDGPrice(state.DGPrices.dg);
 
       const price_json = await Fetch.OLD_DG_PRICE();
-      if(price_json){
-        setOldDGPrice(price_json?.['decentral-games-old']?.usd);
-      }
+      setOldDGPrice(price_json?.['decentral-games-old']?.usd);
     }
 
     fetchData();
