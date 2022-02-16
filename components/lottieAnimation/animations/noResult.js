64e7fa9662348f5891bb870d608f6a373701ddbf
0xsaxit <scott@flux.la>
2022-02-16 17:35:47 +0000

    refactor leaderboard

--- components/lottieAnimation/animations/noResult.js
+++ components/lottieAnimation/animations/noResult.js
@@ -3,23 +3,25 @@ import Lottie from 'react-lottie'
 import loadingData from '../json/doc_no_result.json'
 
 class NoResult extends Component {
-    render() {
-        const loadingOptions = {
-            loop: true,
-            autoplay: true,
-            animationData: loadingData,
-            rendererSettings: {
-                preserveAspectRatio: 'xMidYMid slice'
-            }
-        };
+  render() {
+    const loadingOptions = {
+      loop: true,
+      autoplay: true,
+      animationData: loadingData,
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+      }
+    };
 
-        return (
-            <div className="no-result-div">
-                <Lottie options={loadingOptions} height={200} width={200} />
-                <h3 className="title">No Result</h3>
-            </div>
-        )
-    }
+    return (
+      <div>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <img src="https://res.cloudinary.com/dnzambf4m/image/upload/v1644858496/image_40_woadzu.png" height={200} width={200} />
+        </div>
+        <h3 className="title" style={{ textAlign: 'center' }}>No Result</h3>
+      </div>
+    )
+  }
 }
 
 export default NoResult;
