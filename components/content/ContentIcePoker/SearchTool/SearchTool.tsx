8520024551163af9f67584d68251a4e1b19d9578
alex <alex@decentral.games>
2022-02-16 12:36:43 -0800

    Built out Player Lookup Tool
    AB#2918

--- components/content/ContentIcePoker/SearchTool/SearchTool.tsx
+++ components/content/ContentIcePoker/SearchTool/SearchTool.tsx
@@ -1,13 +1,25 @@
 import React, { FC, ReactElement, useContext, useState } from 'react';
 import { GlobalContext } from '@/store';
-import { Button } from 'semantic-ui-react';
+import { Button, Table } from 'semantic-ui-react';
+import FoxAnimation from 'components/lottieAnimation/animations/fox';
+import HourglassAnimation from 'components/lottieAnimation/animations/hourglass';
+import ModalSearchToolHistory from 'components/modal/ModalSearchToolHistory/ModalSearchToolHistory';
+import Fetch from 'common/Fetch';
 import styles from './SearchTool.module.scss';
 import cn from 'classnames';
+import { useEffect } from 'react';
 
 enum TimePeriods {
   Weekly = 'week',
   Monthly = 'month',
   All = 'all'
+}
+
+export interface GamePlayType {
+  chipsEarned: number;
+  leaderboardPercentile: number;
+  checkedIn: number;
+  numChallengesCompleted: number;
 }
 
 export interface IceSearchToolType {
@@ -20,6 +32,53 @@ const IceSearchTool: FC<IceSearchToolType> = ({ className = '' }: IceSearchToolT
 
   // define local variables
   const [time, setTime] = useState(TimePeriods.Weekly);
+  const [isLoading, setIsLoading] = useState(false);
+  const [gameReports, setGameReports] = useState([]);
+  const [gamePlay, setGamePlay] = useState({ chipsEarned: 0, leaderboardPercentile: 0, checkedIn: 0, numChallengesCompleted: 0 });
+  const [searchAddress, updateSearchAddress] = useState('');
+  const [searchBoxValue, updateSearchBoxValue] = useState('');
+  const [isShowingHistory, setShowingHistory] = useState(false);
+
+  async function searchGameplay(): Promise<void> {
+    if (state.userLoggedIn) {
+      if (!searchBoxValue) {
+        setIsLoading(false);
+        updateSearchAddress('');
+        setGameReports([]);
+        setGamePlay({ chipsEarned: 0, leaderboardPercentile: 0, checkedIn: 0, numChallengesCompleted: 0 });
+      } else {
+        setIsLoading(true);
+        updateSearchAddress(searchBoxValue);
+
+        // Get Gameplay Reports from the API
+        const response = await Fetch.GAMEPLAY_REPORTS(searchBoxValue, time);
+
+        const gamePlay = {
+          chipsEarned:            0,
+          leaderboardPercentile:  0,
+          checkedIn:              0,
+          numChallengesCompleted: 0
+        };
+
+        for (let i = 0; i < response.length; i++) {
+          if (response[i] && Object.keys(response[i].gameplay).length > 0 && Object.getPrototypeOf(response[i]) === Object.prototype) {
+            gamePlay.chipsEarned += response[i].gameplay.chipsEarned;
+            gamePlay.leaderboardPercentile += response[i].gameplay.leaderboardPercentile;
+            gamePlay.checkedIn += 1;
+            gamePlay.numChallengesCompleted += response[i].gameplay.numChallengesCompleted;
+          }
+        }
+
+        setGameReports(response);
+        setGamePlay(gamePlay);
+        setIsLoading(false);
+      }
+    }
+  }
+
+  useEffect(() => {
+    searchGameplay();
+  }, [time]);
 
   function unlockPremium(): ReactElement {
     return (
@@ -74,9 +133,166 @@ const IceSearchTool: FC<IceSearchToolType> = ({ className = '' }: IceSearchToolT
     );
   }
 
+  function searchBox(): ReactElement {
+    return (
+      <section className={styles.search_box}>
+        <input
+          type="text"
+          placeholder="Search for players by ETH address..."
+          value={searchBoxValue}
+          onChange={e => {
+            updateSearchBoxValue(e.target.value);
+          }}
+        />
+        <Button
+          className={styles.search}
+          onClick={() => {
+            searchGameplay();
+          }}
+        >
+          Search
+        </Button>
+      </section>
+    );
+  }
+
+  function lookingForSomeone(): ReactElement {
+    return (
+      <section className={styles.looking_for}>
+        <img src="https://res.cloudinary.com/dnzambf4m/image/upload/v1644858496/image_39_e6ceyn.png" alt="search" />
+        <h1 className={styles.title}>Looking for Someone?</h1>
+        <p className={styles.description}>
+          Search the ICE Poker player database to <br />
+          discover and consider new potential delegates.
+        </p>
+      </section>
+    );
+  }
+
+  function gamePlayTable(): ReactElement {
+    return (
+      <section className={styles.gameplay_table}>
+        <div className={styles.fixed}>
+          <Table fixed unstackable>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell style={{ width: '200px' }}>Player Address</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+              <Table.Row style={{ background: 'rgba(255, 255, 255, 0.08', width: '200px' }}>
+                <Table.Cell className={styles.user_info}>
+                  <img src={`https://events.decentraland.org/api/profile/${searchAddress}/face.png`} alt="avatar" />
+                  <h1>{searchAddress.substr(0, 4) + '...' + searchAddress.substr(-4)}</h1>
+                </Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </Table>
+        </div>
+
+        <div className={styles.scroll}>
+          <Table fixed unstackable>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell style={{ width: '200px' }}>Net Chips Score</Table.HeaderCell>
+                <Table.HeaderCell style={{ width: '230px' }}>Avg.Leaderboard Tier</Table.HeaderCell>
+                <Table.HeaderCell style={{ width: '150px' }}>Check-Ins</Table.HeaderCell>
+                <Table.HeaderCell style={{ width: '220px' }}>Finished Challenges</Table.HeaderCell>
+                <Table.HeaderCell style={{ width: '170px' }}>History</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+              <Table.Row style={{ background: 'rgba(255, 255, 255, 0.08)' }}>
+                {/* Net Chips Score */}
+                <Table.Cell style={{ width: '200px' }}>
+                  <div className={styles.net_chips_score} style={{ textAlign: 'center' }}>
+                    {gamePlay.chipsEarned.toLocaleString()}
+                    <img src="https://res.cloudinary.com/dnzambf4m/image/upload/c_scale,w_210,q_auto:good/v1635212177/FREE_Coin_c08hyk.png" alt="chips" />
+                  </div>
+                </Table.Cell>
+
+                {/* Avg.Leaderboard Tier */}
+                <Table.Cell style={{ width: '230px' }}>
+                  <div className={styles.tier} style={{ textAlign: 'center' }}>
+                    <img src="https://res.cloudinary.com/dnzambf4m/image/upload/c_scale,w_210,q_auto:good/v1637175017/cup_w68eni.png" alt="cup" />
+                    {gamePlay.leaderboardPercentile / gamePlay.checkedIn + 5 <= 50
+                      ? `Top ${Math.round(gamePlay.leaderboardPercentile / gamePlay.checkedIn) + 5}%`
+                      : `Bottom ${100 - Math.round(gamePlay.leaderboardPercentile / gamePlay.checkedIn)}%`}
+                  </div>
+                </Table.Cell>
+
+                {/* Check-Ins */}
+                <Table.Cell style={{ width: '150px' }}>
+                  {gamePlay.checkedIn} of {gameReports.length}
+                </Table.Cell>
+
+                {/* Finished Challenges */}
+                <Table.Cell style={{ width: '220px' }}>
+                  {gamePlay.numChallengesCompleted} of {3 * gameReports.length}
+                </Table.Cell>
+
+                {/* History */}
+                <Table.Cell style={{ width: '170px' }}>
+                  <Button className={styles.breakdown} onClick={() => setShowingHistory(true)}>
+                    See History
+                  </Button>
+                </Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </Table>
+        </div>
+      </section>
+    );
+  }
+
+  function noResult(): ReactElement {
+    return (
+      <section className={styles.no_result}>
+        <img src="https://res.cloudinary.com/dnzambf4m/image/upload/v1644858496/image_40_woadzu.png" alt="search" />
+        <h1 className={styles.title}>Can&apos;t Seem to Find That Player!</h1>
+        <p className={styles.description}>
+          Looks like we don’t have any information yet
+          <br />
+          on that player. Try searching for another.
+        </p>
+      </section>
+    );
+  }
+
   return (
     <section className={`ice-search-tool component ${className}`}>
-      {state.userStatus < 28 ? <section className={styles.playerLookUp}>{playerLookUpHeader()}</section> : unlockPremium()}
+      {!state.userLoggedIn ? (
+        <section className={styles.connect_metamask}>
+          <FoxAnimation />
+        </section>
+      ) : (
+        <>
+          {state.userStatus < 28 ? (
+            <section className={styles.playerLookUp}>
+              {playerLookUpHeader()}
+              {searchBox()}
+
+              {isLoading ? (
+                <section style={{ marginTop: '120px' }}>
+                  <HourglassAnimation />
+                </section>
+              ) : !searchAddress ? (
+                lookingForSomeone()
+              ) : searchAddress && gamePlay.checkedIn > 0 ? (
+                gamePlayTable()
+              ) : (
+                noResult()
+              )}
+            </section>
+          ) : (
+            unlockPremium()
+          )}
+
+          {isShowingHistory ? <ModalSearchToolHistory playerAddress={searchAddress} gameReports={gameReports} setShowingHistory={setShowingHistory} /> : null}
+        </>
+      )}
     </section>
   );
 };
