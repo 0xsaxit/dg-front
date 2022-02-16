c7000ee5713b73756eef666ac1656cba20cb7334
0xsaxit <scott@flux.la>
2022-02-16 17:37:44 +0000

    update noResult image

--- components/lottieAnimation/animations/noResult.js
+++ components/lottieAnimation/animations/noResult.js
@@ -18,7 +18,7 @@ class NoResult extends Component {
         <div style={{ display: 'flex', justifyContent: 'center' }}>
           <img src="https://res.cloudinary.com/dnzambf4m/image/upload/v1644858496/image_40_woadzu.png" height={200} width={200} />
         </div>
-        <h3 className="title" style={{ textAlign: 'center' }}>No Result</h3>
+        <h3 className="title" style={{ textAlign: 'center'}}>No Result</h3>
       </div>
     )
   }
